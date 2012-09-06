dc90b2f3cb9cf12e70a8ba400f3b5b4a257a4c06
Benjamin Dean <bdean@exacttarget.com>
2012-09-06 16:04:44 -0400

    Added unit test scaffolding and example test for candidates to complete

---
+++ tests/jsanddom-test.js
@@ -0,0 +1,14 @@
+/********************************
+Unit Test Example
+********************************/
+module( "Example Unit Test" );
+test( "Example Test", 2, function() {
+    // Verify the method exists
+    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+
+    // Make sure the result from the divide function is valid
+    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+});
+/********************************
+Please create your tests below...
+********************************/
