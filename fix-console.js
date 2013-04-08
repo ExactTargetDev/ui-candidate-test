976fb176e9aff756a26eaed5595f8589de255e7e
Steven Rogers <soldoutactivist@gmail.com>
2013-04-08 01:44:49 -0400

    HTML site plus UX functionality (see notes)
    - Works in modern Chrome, Firefox, Safari, Opera, IE 8/9, iOS Safari. I
    don't have an android phone, so I can't test it.
    - There is an issue with the header on iOS while in portrait mode. The
    two parts of the overlap. If this were a production site, I'd fix that,
    obviously.
    - Towards the end, I didn't clean up my CSS as much as I would
    normally. Nor did I abstract as many common declarations.
    - I didn't copy the individual widgets in the pulse area since that
    wasn't the point.
    - I didn't do any animations for the widget toggling because that
    wasn't specified or even hinted at.
    - I also didn't use jQuery widgets since I was trying to keep things as
    low level as possible.
    - Had I another day, I'd include all the little graphical bits.
    - And though IE8 degrades quite nicely--surprisingly so, it could
    degrade... nicer?
    - I did do the overlay for the help menu, but I'm tired enough I'm not
    going to give it much of a fine dress.
    - People reconstruct that PSD to reasonable HTML in an hour?
    - I didn't do image degrades for several of the elements with gradient
    backgrounds. I demonstrated I could do it several times, which is more
    to the point, I think.
    - The calendar agenda entries are inline-blocked instead of hard-coded
    to look exactly like the mockup.
    - None of the section options (close, settings, etc) work, but that
    wasn't specified either so I didn't worry about that.

---
+++ fix-console.js
@@ -0,0 +1,5 @@
+if (!window.console) window.console = {};
+if (!console.log) console.log = function() {};
+if (!console.warn) console.warn = function() {};
+if (!console.info) console.info = function() {};
+if (!console.error) console.error = function() {};
