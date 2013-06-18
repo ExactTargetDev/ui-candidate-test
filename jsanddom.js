811334dd621b3d5b6c6e12cc058b9ed35783e543
sbarker4183 <sbarker4183@gmail.com>
2013-06-18 15:31:46 -0400

    filled in reverseString function

--- jsanddom.js
+++ jsanddom.js
@@ -1,12 +1,18 @@
      // Example unit test function
      function divide( a, b ) {
         // To see the test pass, uncomment the following line
-        //return a / b;
+        return a / b;
      }
 
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+         if (typeof str === 'string') {
+           var strArr = str.split('');
+           strArr.reverse();
+           return strArr.join('');
+         } else {
+           return str;
+         }
      }
 
      // Write a function that takes an array of numbers and returns the minimum value
