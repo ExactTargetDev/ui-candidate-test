292c23b0df594d206f0721a4287ee4af3d0741ff
Justin R. Evans <jurevans@gmail.com>
2013-07-01 15:05:31 -0400

    Developed wrapper method to interface with Handlebars templates

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -96,50 +96,50 @@ $(document).ready(function(){
 	// Example Calendar Data (Context for rendering Calender via "Handlebars" Templates)
 	var calendar_data = [
 		{
-			month 	: 'June',
-			year 	: '2013',
-			days 	: [
+			month : 'June',
+			year : '2013',
+			days : [
 				{
-					day_of_week		: 'Wednesday',
-					day_of_month 	: '26',
-					calendar_items	: []
+					day_of_week : 'Wed',
+					day_of_month : '26',
+					calendar_items : []
 				},
 				{
-					day_of_week		: 'Thursday',
-					day_of_month 	: '27',
-					calendar_items	: [ { item : 'Event', time : '3:45pm' }, { item : 'Another Event', time : '5:30pm' } ]
+					day_of_week : 'Thur',
+					day_of_month : '27',
+					calendar_items : [ { item : 'Event', time : '3:45pm' }, { item : 'Another Event', time : '5:30pm' } ]
 				},
 				{
-					day_of_week		: 'Friday',
-					day_of_month 	: '28',
-					calendar_items	: [ { item : 'End of iteration', time : '6:00pm' } ]
+					day_of_week : 'Fri',
+					day_of_month : '28',
+					calendar_items : [ { item : 'End of iteration', time : '6:00pm' } ]
 				},
 				{
-					day_of_week		: 'Saturday',
-					day_of_month 	: '29',
-					calendar_items	: [ { item : 'Event at the place', time : '3:30pm' } ]
+					day_of_week : 'Sat',
+					day_of_month : '29',
+					calendar_items : [ { item : 'Event at the place', time : '3:30pm' } ]
 				},
 				{
-					day_of_week		: 'Sunday',
-					day_of_month 	: '30',
-					calendar_items	: []
+					day_of_week : 'Sun',
+					day_of_month : '30',
+					calendar_items : []
 				}
 			]
 		},
 		{
-			month 	: 'July',
-			year	: '2013',
-			days	: [
+			month : 'July',
+			year : '2013',
+			days : [
 				
 				{
-					day_of_week		: 'Monday',
-					day_of_month 	: '1',
-					calendar_items	: [ { item : 'Stand-up', time : '9:00am' }, { item : 'Planning Meeting', time : '10:30am' } ]
+					day_of_week : 'Mon',
+					day_of_month : '1',
+					calendar_items : [ { item : 'Stand-up', time : '9:00am' }, { item : 'Planning Meeting', time : '10:30am' } ]
 				},
 				{
-					day_of_week		: 'Tuesday',
-					day_of_month 	: '2',
-					calendar_items	: []
+					day_of_week : 'Tue',
+					day_of_month : '2',
+					calendar_items : []
 				}
 			]
 		}
