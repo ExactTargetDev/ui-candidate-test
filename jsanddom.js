50807a4b2f936ec7bc3f838ed4d9e753366e1ee9
joshforward <joshforward@gmail.com>
2013-07-16 22:57:50 -0400

    Almost done with the js portion
    Created meta.txt
    
    Have filled in most of the jsanddom functions, adding a bunch of unit
    tests as I go. Have not been using jQuery or other libraries to show I
    understand the processes that jQuery makes easy for us. Have been
    testing as I go in FF, Chrome, IE9.

--- jsanddom.js
+++ jsanddom.js
@@ -6,17 +6,26 @@
 
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+         return str.split('').reverse().join('');
      }
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+         return Math.min.apply(null, values);
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
-         // FILL THIS IN
+	 
+         var retArray = [];
+		
+		// cache values.length to avoid a lookup penalty every loop
+		for (var i=0, cnt=values.length; i < cnt; i++)
+		 {
+			if (retArray.indexOf( values[i] ) < 0)
+				retArray.push(values[i]);
+		 }
+		 return retArray;
      }
 
      // Write a function that logs the numbers from 1 to 100 to the console.
@@ -24,7 +33,24 @@
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
-         // FILL THIS IN
+	 
+        for (var i=1, str=null; i <= 100; i++)
+		{
+			// clear out tmp var before use
+			str = '';
+			
+			if ( i%3 == 0 )
+				str += 'Fizz';
+			
+			if ( i%5 == 0 )
+				str += 'Buzz';
+			
+			if ( str.length == 0 )
+				str = i.toString();
+			
+			console.log(str);
+		}
+		 
      }
 
      // You have a master array of strings, where each element is a fruit name.
@@ -32,7 +58,16 @@
      // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
      function removeFruits(fruits, fruitsToRemove) {
-         // FILL THIS IN
+         
+		 var retArray = [];
+		 for (var i=0, cnt=fruits.length; i < cnt; i++)
+		 {
+			if ( fruitsToRemove.indexOf(fruits[i]) < 0 )
+				retArray.push( fruits[i] );
+		 }
+		 
+		 return retArray;
+		 
      }
 
      // Write a function to push either a simple value or an array of values onto a specified array.
@@ -40,26 +75,63 @@
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+		
+		// type checking for an array, as suggested by ecma docs:
+        var isArray = function (val) {
+				// since typeof [] == "object", have to get a bit trickier...
+				// (this call returns "[object Array]")
+				return (Object.prototype.toString.call([]) == Object.prototype.toString.call(val));
+			};
+		
+		
+		// if single value, wrap in array so we can call .push() in a single place in this func
+		if ( !isArray(toPush) )
+			toPush = [toPush];
+		
+		for (var i=0,cnt=toPush.length; i < cnt; i++)
+		{
+			array.push( toPush[i] );
+		}
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
-         // FILL THIS IN
+         var retVal = null;
+		 if (0 == sourceStr.length)
+			retVal = [];
+		else
+			retVal = sourceStr.split(',');
+		
+		return retVal;
      }
 
      // Write a function that will take any number of arguments and return their sum
      function sum() {
-         // FILL THIS IN
+		var total = 0;
+        for (var i=0, cnt=arguments.length; i < cnt; i++)
+			total += arguments[i];
+		
+		return total;
      }
 
      // Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
-         // FILL THIS IN
+         return (sourceStr.search(/^\s+$/) == 0);
      }
 
      // write an example of a javascript closure
+		// the document title will be logged to the console
+	 function closureExample() {
+		var documentTitle = document.querySelector('title').textContent,
+			callback = function(e) {
+				console.log( 'closureExample(): ' + documentTitle );
+			};
+		
+		window.addEventListener('click', callback, false);
+	 }
+	 closureExample();
 
+	 
      // define a json object that represents a collection of people.
      // each person should have the following properties
      // - first name
@@ -68,6 +140,47 @@
      // - state
      // - zip
      // - a collection of phone numbers (home, work, mobile)
+	 var peopleCollection = {
+		// personID 21
+		'21': {
+			'firstName': 'Chester',
+			'lastName': 'Arthur',
+			'city': 'Fairfield',
+			'state': 'Vermont',
+			'zip': '05455',
+			'phone': {
+				'home': '123-456-7890',
+				'work': '321-654-0987',
+				'mobile': '456-789-0123'
+			}
+		},
+		// personID 22
+		'22': {
+			'firstName': 'Grover',
+			'lastName': 'Cleveland',
+			'city': 'Caldwell',
+			'state': 'New Jersey',
+			'zip': '07007',
+			'phone': {
+				'home': '654-987-3210',
+				'work': '789-012-3456',
+				'mobile': '987-210-6543'
+			}
+		},
+		// personID 23
+		'23': {
+			'firstName': 'Benjamin',
+			'lastName': 'Harrison',
+			'city': 'North Bend',
+			'state': 'Ohio',
+			'zip': '45052',
+			'phone': {
+				'home': '111-222-3333',
+				'work': '444-555-6666',
+				'mobile': '777-888-9999'
+			}
+		}
+	 };
 
 
      // Create a javascript object (DataTable) with the following:
@@ -81,16 +194,125 @@
      // .addColumns('column1', 'column2', 'column3');
      // .addRow('value1A', 'value1B', 'value1C');
      // .addRow('value2A', 'value2B', 'value2C');
+	 
+	/*
+	Example usage:
+	
+	 var dt = new DataTable();
+	 dt.addColumns('A', 'B', 'C');
+	 dt.addRow('1', '2', '3');
+	 dt.addRow('4', '5', '6');
+	 dt.addRow('7', '8', '9');
+	 
+	 console.log( dt.getData() );
+	*/
+	 var DataTable = function() {
+		// constructor
 
+		// don't want these to be accessible outside the object
+		var columns = [],
+			rows = [];
+		
+		// in order to make the row/col arrays private, bind at creation
+		this.addColumns = this._addItems.bind(this, columns);
+		this.addRow     = this._addItems.bind(this, rows);
+		this.getData    = this._getData.bind(this, columns, rows);
+	 };
+
+	 // used by .addRows() and .addColumns()
+	 DataTable.prototype._addItems = function(arr /*, item, item, item, ... */){
+		for (var i=1, cnt=arguments.length; i < cnt; i++) {
+			arr.push(arguments[i]);
+		}
+	 };
+
+	// used by .getData()
+	 DataTable.prototype._getData = function(columns, rows){
+		// build json response
+		// ex: {'col1': 'first name', 'col2': 'last name', ...}
+		
+		var returnJSON = {},
+			numColumns = columns.length;
+		for (var i=0, rowCnt=Math.min(rows.length/numColumns); i < rowCnt; i++)
+		{
+			returnJSON[i] = {};
+			for (var j=0, colCnt=columns.length; j < colCnt; j++)
+			{
+				returnJSON[i][ columns[j] ] = rows[(i*numColumns) + j];
+			}
+		}
+		return returnJSON;
+	 };
+
+	 
+	 
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
+	 function createSelectInDiv1()
+	 {
+		var container = document.getElementById('div1'),
+			selectBox = document.createElement('select'),
+			optionsToCreate = {
+				'Foo': 'bar',
+				'Marco': 'polo',
+				'Life, The Universe, Everything': '42'
+			},
+			theButton = document.createElement('button'),
+			theButtonClickHandler = function(e){
+				e.preventDefault();
+				
+				var selectedOption = selectBox.options[ selectBox.selectedIndex ];
+				
+				console.log('Selected option === Name: "' + selectedOption.textContent + '", Value: "'+selectedOption.value+'"');
+			};
+		
+		// don't try to run if we're not on the page with #div1
+		if (!container)
+			return;
+		
+		// first, create the options and add to the select
+		for (var name in optionsToCreate)
+		{
+			// create option, text node for label
+			var opt = document.createElement('option'),
+				txt = document.createTextNode( name );
+			
+			// set val, label
+			opt.setAttribute('value', optionsToCreate[name]);
+			opt.appendChild( txt );
+			
+			// add to select
+			selectBox.appendChild( opt );
+		}
+		
+		// next, attach click handler for the button
+		theButton.appendChild( document.createTextNode('Send To Console') );
+		theButton.addEventListener('click', theButtonClickHandler, false);
+		
+		// finally, add this stuff to the page
+		container.appendChild(selectBox);
+		container.appendChild(theButton);
+	 }
+	 createSelectInDiv1();
+	 
+	 
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
+	 
+	 // Answers:
+	 // $('a.link')
+	 // $('#fizz a')
+	 // $('#fizz .link')
+	 //	$('.bar .link')
+	 // $('.buzz > a')
 
+	 
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
+	 function createListItems(numItemsToGenerate) {
+	 }
 
      // Use javascript to add a list of checkboxes and 2 links
      // to the div with an id of "foobar"
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,6 +1,7 @@
 /********************************
 Unit Test Example
 ********************************/
+/*
 module( "Example Unit Test" );
 test( "Example Test", 2, function() {
     // Verify the method exists
@@ -9,6 +10,317 @@ test( "Example Test", 2, function() {
     // Make sure the result from the divide function is valid
     ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
 });
+*/
+
+// helper function
+function isArray(val) {
+	// since typeof [] == "object", have to get a bit trickier
+	// this call returns "[object Array]"
+	return (Object.prototype.toString.call([]) == Object.prototype.toString.call(val));
+};
+
 /********************************
 Please create your tests below...
 ********************************/
+
+module( 'Reverse String Tests' );
+test( 'Basic Test: String of letters', 9, function() {
+
+	ok( 'reverseString' in window, 'reverseString() found.' );
+	
+	function runTests(inStr, outStr)
+	{
+		var output = reverseString(inStr);
+		
+		equal( typeof output, 'string', 'Expected type a string to be returned, got: ' + typeof output );
+		equal( output,  outStr, 'Expected "' + outStr + '", got "' + output + '"');
+	}
+	
+	// test letters...
+	runTests('gfedcba', 'abcdefg');
+	runTests('abcdefg', 'gfedcba');
+	
+	// a string of numbers...
+	runTests('12345', '54321');
+	
+	// letters, spaces, punctuation
+	runTests('The "quick" brown fox...', '...xof nworb "kciuq" ehT');
+	
+});
+
+
+
+module('Find minimum value in array of numbers');
+test( 'Basic Tests', 7, function() {
+	
+	ok( 'findMinValue' in window, 'findMinValue() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inputArray, expectedOutput)
+	{
+		var output = findMinValue(inputArray);
+		
+		equal( typeof output, 'number', 'Expected type "number" to be returned, got: ' + typeof output );
+		equal( output,  expectedOutput, 'Expected "' + expectedOutput + '", got "' + output + '"');
+	}
+	
+	// sorted array, asc & desc
+	runTest([1,2,3,4,5], 1);
+	runTest([5,4,3,2,1], 1);
+	
+	// array of random numbers
+	runTest([1024,60,50,75,99,42,184], 42);
+	
+});
+
+
+
+module('Find unique values in an array');
+test( 'Basic Tests', 7, function() {
+	
+	ok( 'findDistinctValues' in window, 'findDistinctValues() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inputArray, expectedOutput)
+	{
+		var output = findDistinctValues(inputArray);
+		
+		ok( isArray(output), 'Expected returned value to be an array: ' + isArray(output).toString() );
+		deepEqual( output,  expectedOutput, 'Expected [' + expectedOutput.join(', ') + '], got [' + output.join(', ') + ']');
+	}
+	
+	// all the same value
+	runTest([1,1,1,1,1], [1]);
+	
+	// all different values
+	runTest([1,2,3,4,5], [1,2,3,4,5]);
+	
+	// mix of unique & repeated values
+	runTest([1,1,1,2,3,4,4,5,"Javascript","Javascript","Javascript",10,"?!"], [1,2,3,4,5,"Javascript",10,"?!"]);
+	
+});
+
+
+
+module('FizzBuzz');
+test( 'Basic Tests', 1, function() {
+
+	ok( 'doFizzBuzz' in window, 'doFizzBuzz() found.' );
+
+});
+
+
+
+module('Remove value from an array');
+test( 'Basic Tests', 7, function() {
+	
+	ok( 'removeFruits' in window, 'removeFruits() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inputArray, removeArray, expectedOutput)
+	{
+		var output = removeFruits(inputArray, removeArray);
+		
+		ok( isArray(output), 'Expected returned value to be an array: ' + isArray(output).toString() );
+		deepEqual( output,  expectedOutput, 'Expected [' + expectedOutput.join(', ') + '], got [' + output.join(', ') + ']');
+	}
+	
+	// remove one item
+	runTest(["Apple", "Orange", "Grape", "Pear", "Plum"], ["Orange"], ["Apple", "Grape", "Pear", "Plum"]);
+	
+	// remove three
+	runTest(["Apple", "Orange", "Grape", "Pear", "Plum"], ["Pear", "Orange", "Apple"], ["Grape", "Plum"]);
+
+	// remove three, one item in remove array does not exist in original array
+	runTest(["Apple", "Orange", "Grape", "Pear", "Plum"], ["Pear", "Orange", "Blackberry"], ["Apple", "Grape", "Plum"]);
+	
+});
+
+
+
+module('Push onto an array');
+test( 'Basic Tests', 5, function() {
+	
+	ok( 'pushOntoArray' in window, 'pushOntoArray() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inputArray, toPush, expectedOutput)
+	{
+		var originalArrayLength = inputArray.length,
+			numOfItemsToPush = (isArray(toPush)) ? toPush.length:1,
+			expectedArrayLength = originalArrayLength + numOfItemsToPush;
+		
+		pushOntoArray(inputArray, toPush);
+		
+		equal( inputArray.length, expectedArrayLength, 'New expected length of array is '+expectedArrayLength+', got: ' + inputArray.length.toString() );
+		deepEqual( inputArray,  expectedOutput, 'Expected [' + expectedOutput.join(', ') + '], got [' + inputArray.join(', ') + ']');
+	}
+	
+	// start out with an array
+	var inputArray = [42, "So long"];
+	
+	// test pushing a single item
+	runTest( inputArray, 256, [42, "So long", 256] );
+	
+	// test pushing an array
+	runTest( inputArray, ["Thanks", 4096, 5, Math.PI], [42, "So long", 256, "Thanks", 4096, 5, Math.PI] );
+	
+});
+
+
+
+module('Split string by commas');
+test( 'Basic Tests', 9, function() {
+	
+	ok( 'splitListStrUsingComma' in window, 'splitListStrUsingComma() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inputString, expectedOutput)
+	{
+		var output = splitListStrUsingComma(inputString);
+		
+		ok( isArray(output), 'Expected returned value to be an array: ' + isArray(output).toString() );
+		deepEqual( output,  expectedOutput, 'Expected [' + expectedOutput.join(', ') + '], got [' + output.join(', ') + ']');
+	}
+	
+	// general tests with numbers, letters, words, other punc
+	runTest('1,2,3', ['1','2','3']);
+	runTest('Javascript, jQuery, prototype', ['Javascript',' jQuery',' prototype']);
+	runTest('?,/,!,a,b,c, ,42', ['?','/','!','a','b','c',' ','42']);
+	
+	// special case of empty string
+	runTest('', []);
+	
+});
+
+
+
+module('Sum arbitrary number of arguments passed to a function');
+test( 'Basic Tests', 9, function() {
+	
+	ok( 'sum' in window, 'sum() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(funcArguments, expectedOutput)
+	{
+		var output = sum.apply(null, funcArguments),
+			outputType = typeof output;
+		
+		equal( outputType, 'number', 'Expected returned value to be of type "number", got: ' + outputType );
+		equal( output,  expectedOutput, 'Expected (' + funcArguments.join(' + ') + ') = ' + expectedOutput + ', got: ' + output);
+	}
+	
+	// general test of a 1/multiple args
+	runTest([1], 1);
+	runTest([1,2,3], 6);
+	runTest([1,2,3,2,1,10], 19);
+	runTest([1,1,1,-1,1,2,-1,1,1,0,1,3], 10);
+	
+});
+
+
+
+module('Check if string is only whitespace');
+test( 'Basic Tests', 15, function() {
+	
+	ok( 'isOnlyWhitespace' in window, 'isOnlyWhitespace() found.' );
+	
+	// make it easier to run a few similar tests in a row...
+	function runTest(inStr, expectedOutput)
+	{
+		var output = isOnlyWhitespace(inStr),
+			outputType = typeof output;
+		
+		equal( outputType, 'boolean', 'Expected returned value to be of type "boolean", got: ' + outputType );
+		equal( output,  expectedOutput, 'Expected ' + expectedOutput.toString() + ', got: ' + output.toString());
+	}
+	
+	// general test of various strings
+	runTest('', false);
+	runTest(' ', true);
+	runTest('asdf', false);
+	runTest('     ', true);
+	runTest('	', true);
+	runTest("\n\t\n", true);
+	runTest("\nasdf\n", false);
+	
+});
+
+
+
+
+module('DataTable tests');
+test( 'Testing if initializied correctly', 5, function() {
+	
+	ok( 'DataTable' in window, 'DataTable found.' );
+	
+	var dt = null,
+		createdWithoutError = true;
+
+	try {
+		dt = new DataTable();
+	} catch (err) {
+		createdWithoutError = false;
+	}
+	
+	ok( createdWithoutError, 'DataTable object created successfully.' );
+	
+	ok( 'addColumns' in dt, 'DataTable.addColumns() bound successfully.' );
+	ok( 'addRow' in dt, 'DataTable.addRow() bound successfully.' );
+	ok( 'getData' in dt, 'DataTable.getData() bound successfully.' );
+	
+});
+test( 'Testing write capability', 2, function() {
+		
+	var dt = new DataTable(),
+		addedColumnsSuccessfully = true,
+		addedRowsSuccessfully = true;
+	
+	try {
+		dt.addColumns('A', 'B', 'C');
+	} catch (err) {
+		addedColumnsSuccessfully = false;
+	}
+	
+	ok( addedColumnsSuccessfully, 'DataTable.addColumns() called successfully.' );
+	
+	try {
+		dt.addRow('1', '2', '3');
+	} catch (err) {
+		addedRowsSuccessfully = false;
+	}
+	
+	ok( addedRowsSuccessfully, 'DataTable.addRow() called successfully.' );
+	
+});
+test( 'Testing read capability', 10, function() {
+		
+	var dt = new DataTable(),
+		gotDataSuccessfully = true;
+	
+	dt.addColumns('A', 'B', 'C');
+	dt.addRow('1', '2', '3');
+	dt.addRow('4', '5', '6');
+	dt.addRow('7', '8', '9');
+	
+	var output = null;
+	try {
+		output = dt.getData();
+	} catch (err) {
+		gotDataSuccessfully = false;
+	}
+	
+	ok( gotDataSuccessfully, 'DataTable.getData() called successfully.' );
+	
+	equal( output[0]['A'], '1', 'Expected [Row0,ColA] = 1, got: ' + output[0]['A']);
+	equal( output[0]['B'], '2', 'Expected [Row0,ColB] = 2, got: ' + output[0]['B']);
+	equal( output[0]['C'], '3', 'Expected [Row0,ColC] = 3, got: ' + output[0]['C']);
+	
+	equal( output[1]['A'], '4', 'Expected [Row1,ColA] = 4, got: ' + output[1]['A']);
+	equal( output[1]['B'], '5', 'Expected [Row1,ColB] = 5, got: ' + output[1]['B']);
+	equal( output[1]['C'], '6', 'Expected [Row1,ColC] = 6, got: ' + output[1]['C']);
+	
+	equal( output[2]['A'], '7', 'Expected [Row2,ColA] = 7, got: ' + output[2]['A']);
+	equal( output[2]['B'], '8', 'Expected [Row2,ColB] = 8, got: ' + output[2]['B']);
+	equal( output[2]['C'], '9', 'Expected [Row2,ColC] = 9, got: ' + output[2]['C']);
+});
