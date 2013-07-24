c803103e8e92616c57b0481e18c7768a113986a0
Jeremy Barngrover <jcbarngrover@gmail.com>
2013-07-24 03:43:51 -0400

    Final Push
    clean up and made sure to finish what needed to be done.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-
+	$('.pulsePanel').click(function(e){
+		e.preventDefault();
+		if(!$(this).hasClass('bigBox')){
+			var bigBox = $('.bigBox');
+			var cur = $(this);
+			bigBox.removeClass('bigBox');
+			cur.addClass('bigBox');
+		}
+	});
+	
+	$('.helpLink').click(function(e){
+		e.preventDefault();
+		$.ajax({
+			url : 'data/help.json',
+			dataType: 'json',
+			success : function(d){
+				for(var a =0; a < d.help.length; a++){
+					var link = '<a href="'+ d.help[a].URL +'" >'+d.help[a].title+'</a><br/>';
+					console.log(link);
+					$('.overlayContent').append('<a href="'+ d.help[a].URL +'" >'+d.help[a].title+'</a><br/>');
+					
+				}
+			}
+		});
+		$('.overlay').fadeToggle(300);
+	});
+	$('.overlay').click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.overlayContent').empty();
+		$('.overlay').fadeToggle(300);
+	});
 });
