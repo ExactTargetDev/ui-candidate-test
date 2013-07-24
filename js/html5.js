ed7387ff88f2d25470bf8863b55e7d46711709d7
Jeremy Barngrover <jcbarngrover@gmail.com>
2013-07-24 00:24:14 -0400

    Basic styling of Screenshot.html
    added reset and html 5 crutches for IE 8. laid out header and starting
    side nav.

---
+++ js/html5.js
@@ -0,0 +1,6 @@
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('nav');
