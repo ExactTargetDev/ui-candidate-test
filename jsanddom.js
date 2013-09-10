7a10190c6e9100c6bf65061d2c8a7ab821ea81f5
SolidlyStated <evan@solidlystated.com>
2013-09-10 16:43:15 -0400

    finished script

--- jsanddom.js
+++ jsanddom.js
@@ -1,4 +1,9 @@
-     // Example unit test function
+     // Evan Meagher on 9/10/2013
+	 // using good ole native JS with no framework.
+	 // a little more verbose than a lot of the helpful JQuery functions :)
+	 
+	 
+	 // Example unit test function
      function divide( a, b ) {
         // To see the test pass, uncomment the following line
         return a / b;
@@ -76,17 +81,34 @@
 
      // Write a function that will take any number of arguments and return their sum
      function sum() {
-         // FILL THIS IN
+		var total = 0;
+		for (var i=0; i<arguments.length; i++)
+		{
+			if(!isNaN(parseFloat(arguments[i])) && isFinite(arguments[i]))
+			{
+				total+=arguments[i];
+			}
+		}
+		return total;
      }
 
      // Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
-         // FILL THIS IN
+         return (sourceStr.trim() == "");
      }
 
      // write an example of a javascript closure
-
-     // define a json object that represents a collection of people.
+	 // adding events in a loop, from my article in 2010 
+	 // http://solidlystated.com/scripting/javascript-add-event-in-a-loop/
+	function runme() {
+		for(var i=1; i<10; i++){ 
+			someElement.onclick = function(a1,a2){
+				return function(){ myNewFunction(a1,a2); }
+			}(arg1,arg2);
+		}
+	}
+	
+	 // define a json object that represents a collection of people.
      // each person should have the following properties
      // - first name
      // - last name
@@ -94,7 +116,19 @@
      // - state
      // - zip
      // - a collection of phone numbers (home, work, mobile)
-
+	
+	var jsonobj = {
+    "first name": "Evan",
+    "last name": "Meagher",
+    "city": "Indianapolis",
+    "state": "Indiana",
+    "zip": 46220,
+	"phone": {
+		"home": "317-555-5555",
+		"work": "317-555-5555",
+		"mobile": "317-555-5555",
+		}
+	}
 
      // Create a javascript object (DataTable) with the following:
      // A private columns property (string array)
@@ -107,18 +141,144 @@
      // .addColumns('column1', 'column2', 'column3');
      // .addRow('value1A', 'value1B', 'value1C');
      // .addRow('value2A', 'value2B', 'value2C');
+	 
+	DataTable = new Object();
+	 
+	DataTable.columns = new Array();
+	DataTable.rows = new Array();
+	
+	DataTable.addRows = function() {
+		for (var i=0; i<arguments.length; i++)
+		{
+			DataTable.rows.push(arguments[i]);
+		}
+	}
+	DataTable.addColumns = function() {
+		for (var i=0; i<arguments.length; i++)
+		{
+			DataTable.columns.push(arguments[i]);
+		}
+	}
+	DataTable.getData = function(){
+		
+		// TEST DATA (TEMPORARY)
+		DataTable.addColumns('column1', 'column2', 'column3');
+		DataTable.addRows('value1A', 'value1B', 'value1C');
+		DataTable.addRows('value2A', 'value2B', 'value2C');
+		
+		
+		var retVal = new Object();
+		var numCols = DataTable.columns.length; 
+		var numRows = DataTable.rows.length; 
+		for (var i=0; i<numRows; i++)
+		{
+			/* NEED TO REFACTOR SO IT ADDS TO CORRECT COLS BEYOND 3
+			NOT SURE HOW THE RETURNED OBJECT SHOULD LOOK, BUT OUTTA TIME
+			AND DON'T WANNA KEEP ANYONE WAITING ANY LONGER
+			*/ 
+			retVal[DataTable.columns[i]] = DataTable.rows[i];
+		}
+		return retVal;
+	}
 
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
 
+	 var selEl = document.createElement("select");
+	 selEl.setAttribute("id", "selEl");
+	 document.getElementById("div1").appendChild(selEl);
+	 
+	 var opt1 = document.createElement("option");
+	 opt1.setAttribute("value", "apples");
+	 opt1.setAttribute("name", "namedapples");
+	 opt1.innerHTML = "apples";
+	 document.getElementById("selEl").appendChild(opt1);
+	 var opt2 = document.createElement("option");
+	 opt2.setAttribute("value", "bananas");
+	 opt2.setAttribute("name", "namedbananas");
+	 opt2.innerHTML = "bananas";
+	 document.getElementById("selEl").appendChild(opt2);
+	 
+	 var selBtn = document.createElement("input");
+	 selBtn.setAttribute("id", "selBtn");
+	 selBtn.setAttribute("type", "button");
+	 selBtn.setAttribute("value", "Log Choice");
+	 document.getElementById("div1").appendChild(selBtn);
+	 
+	 selBtn.onclick = function(){
+		var box = document.getElementById("selEl");
+		var elvalue = box.options[box.selectedIndex].value;
+		var elname = box.options[box.selectedIndex].attributes.getNamedItem("name").value;
+		console.log("name = "+elname+" value = "+elvalue);
+	 }
+	 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
-
+	 
+	 // HEY you said we didnt need to use a framework... but no sweat. 	 
+	 /*
+	 $("a");
+	 $(".link");
+	 $("#fizz:first-child");
+	 $("contains('sample anchor')");
+	 $("[href]");
+	 */
+	 
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
 
+	 var ulEl = document.createElement("ul");
+	 ulEl.setAttribute("id", "list1");
+	 document.body.appendChild(ulEl);
+	 
+	 var items = ["corvettes","apples","bananas","oranges","pears"]; // use an array literal here for fun
+	 for(var i = 0; i<items.length; i++)
+	 {
+		var liEl = document.createElement("li");
+		liEl.innerHTML = items[i];
+		document.getElementById("list1").appendChild(liEl);
+	 }
+	 
      // Use javascript to add a list of checkboxes and 2 links
      // to the div with an id of "foobar"
      // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
      // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
+	 var box1 = document.createElement("input");
+	 var box2 = document.createElement("input");
+	 var box3 = document.createElement("input");
+	 box1.setAttribute("type", "checkbox");
+	 box2.setAttribute("type", "checkbox");
+	 box3.setAttribute("type", "checkbox");
+	 box1.setAttribute("class", "checks");
+	 box2.setAttribute("class", "checks");
+	 box3.setAttribute("class", "checks");
+	 document.getElementById("foobar").appendChild(box1);
+	 document.getElementById("foobar").appendChild(box2);
+	 document.getElementById("foobar").appendChild(box3);
+	 
+	 var link1 = document.createElement("a");
+	 var link2 = document.createElement("a");
+	 link1.setAttribute("id","link1");
+	 link2.setAttribute("id","link2");
+	 link1.setAttribute("href","#");
+	 link2.setAttribute("href","#");
+	 link1.innerHTML = " Check All";
+	 link2.innerHTML = " Uncheck All";
+	 document.getElementById("foobar").appendChild(link1);
+	 document.getElementById("foobar").appendChild(link2);
+	 
+	 var boxes = document.getElementsByClassName("checks");
+	 
+	 link1.onclick = function(){
+		for(var i = 0; i < boxes.length; i++)
+		{
+			boxes[i].checked = true;
+		}
+	 }
+	 link2.onclick = function(){
+		for(var i = 0; i <boxes.length; i++)
+		{
+			boxes[i].checked = false;
+		}
+	 }
