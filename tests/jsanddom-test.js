2e122d50444c8901f2c152985383b4364431fab0
Niraj Venkat <venkatn@purdue.edu>
2013-10-07 23:04:28 -0400

    Created test cases for reverseString and findMinValue

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,20 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+module( "Reverse String" );
+test( "Reverse String Test", 2, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' ); 
+
+    // Make sure the result from the divide function is valid
+    ok( reverseString( 'A quick brown fox' ), 'xof nworb kciuq A', 'Expected "xof nworb kciuq A" as the result, the result was: ' +  reverseString( 'A quick brown fox' ) );
+});
+
+module( "Find Min Value" );
+test( "Find Min Value Test", 2, function() {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' ); 
+
+    // Make sure the result from the divide function is valid
+    ok( findMinValue( [1, 2, -1, -6, Infinity, 5, 100.0, -6.1, 0, -6, -12] ),  -12, 'Expected -12 as the result, the result was: ' +   findMinValue( [1, 2, -1, -6, Infinity, 5, 100.0, -6.1, 0, -6, -12] ) );
+});
