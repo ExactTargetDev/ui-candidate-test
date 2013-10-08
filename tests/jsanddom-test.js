1457c966af7b3873ab5680414c347292cdf641b2
Niraj Venkat <venkatn@purdue.edu>
2013-10-07 23:53:18 -0400

    Finished up test cases
    I have finished the UI candidate test to the best of my ability. Unfortunately since I am not a web dev, having never worked with JavaScript before cost me a lot of time on the tests. I have completed what I can in one hour.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -18,7 +18,7 @@ test( "Reverse String Test", 2, function() {
     equal( typeof reverseString, 'function', 'Must contain a reverseString function' ); 
 
     // Make sure the result from the divide function is valid
-    ok( reverseString( 'A quick brown fox' ), 'xof nworb kciuq A', 'Expected "xof nworb kciuq A" as the result, the result was: ' +  reverseString( 'A quick brown fox' ) );
+    ok( reverseString( 'The quick brown fox' ), 'xof nworb kciuq ehT', 'Expected "xof nworb kciuq ehT" as the result, the result was: ' +  reverseString( 'A quick brown fox' ) );
 });
 
 module( "Find Min Value" );
@@ -37,4 +37,39 @@ test( "Find Distinct Values Test", 2, function() {
 
     // Make sure the result from the divide function is valid
     ok( findDistinctValues( [1, 2, 1, -6, Infinity, 5, 100.0, -6.1, 0, -6, -12, -12, Infinity] ), [1, 2, -6, Infinity, 5, 100.0, -6.1, 0, -12], 'Expected "[1, 2, -6, Infinity, 5, 100.0, -6.1, 0, -12]" as the result, the result was: ' +  findDistinctValues( [1, 2, 1, -6, Infinity, 5, 100.0, -6.1, 0, -6, -12, -12, Infinity] ) );
+});
+
+module( "Remove Fruits" );
+test( "Remove Fruits Test", 2, function() {
+    // Verify the method exists
+    equal( typeof removeFruits, 'function', 'Must contain a removeFruits function' ); 
+
+    // Make sure the result from the divide function is valid
+	var fruits = ['apple', 'banana', 'grapes', 'strawberry', 'pear'];
+	var fruitsToRemove = ['pear', 'banana'];
+    ok( removeFruits( fruits, fruitsToRemove ), ['apple', 'grapes', 'strawberry'], 'Expected "["apple", "grapes", "strawberry"]" as the result, the result was: ' +  removeFruits( fruits, fruitsToRemove ));
+});
+
+module( "Push onto Array" );
+test( "Push onto Array Test", 3, function() {
+    // Verify the method exists
+    equal( typeof pushOntoArray, 'function', 'Must contain a pushOntoArray function' ); 
+
+    // Make sure the result from the divide function is valid
+	var array = [1, 2, 3, 4, 5];
+	var pushArray = [7, 8];
+	var pushItem = 6;
+    ok( pushOntoArray( array, pushitem ), [1, 2, 3, 4, 5, 6], 'Expected "[1, 2, 3, 4, 5, 6]" as the result, the result was: ' +  pushOntoArray( array1, item ));
+    ok( pushOntoArray( array, pushArray ), [1, 2, 3, 4, 5, 7, 8,], 'Expected "[1, 2, 3, 4, 5, 7, 8]" as the result, the result was: ' +  pushOntoArray( array1, array2));
+});
+
+module( "Split String" );
+test( "Push onto Array Test", 2, function() {
+    // Verify the method exists
+    equal( typeof splitListStrUsingComma, 'function', 'Must contain a splitListStrUsingComma function' ); 
+
+    // Make sure the result from the divide function is valid
+	var string = 'The, quick, brown, fox';
+    ok( splitListStrUsingComma( str ), ['The', ' quick', ' brown', ' fox'], 'Expected "["The", " quick", " brown", " fox"]" as the result, the result was: ' +  splitListStrUsingComma( str ));
+    
 });
