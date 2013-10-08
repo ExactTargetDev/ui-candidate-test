d3f12e30f0f7d9ce3d5b6a2440d607b755a055f9
Niraj Venkat <venkatn@purdue.edu>
2013-10-07 22:53:37 -0400

    Finished divide and reverseString

--- jsanddom.js
+++ jsanddom.js
@@ -1,12 +1,16 @@
      // Example unit test function
      function divide( a, b ) {
         // To see the test pass, uncomment the following line
-        //return a / b;
+        return a / b;
      }
 
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+         var reverse = '';
+		 for(var i = str.length; i > 0; i--){
+			reverse += str[i];
+		 }
+		 return reverse;
      }
 
      // Write a function that takes an array of numbers and returns the minimum value
