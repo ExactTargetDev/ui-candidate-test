5da0d38e94d90327ca854cd862afbdf731d3811f
rostag <rostislav.siryk@gmail.com>
2013-10-21 18:01:05 +0300

    doFizzBuzz implemented, misprints corrected.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -18,6 +18,7 @@ test( "reverseString Test", 5, function() {
     equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
 
     // Check the reverseString function works right
+    // First test is taken from data/testdata.txt:
     equal( reverseString( "Subscribers rock" ), "kcor srebircsbuS", 'Expected "kcor srebircsbuS" as the result, the result was: ' + reverseString( "Subscribers rock" ) );
     equal( reverseString( "123" ), "321", 'Expected "321" as the result, the result was: ' + reverseString( "123" ) );
     equal( reverseString( " a#X -12 3" ), "3 21- X#a ", 'Expected "3 21- X#a " as the result, the result was: ' + reverseString( " a#X -12 3" ) );
@@ -29,6 +30,7 @@ test( "findMinValue Test", 3, function() {
     equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' );
 
     // Check the findMinValue function works right
+    // First test is taken from data/testdata.txt:
     equal( findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ), -1.1, 'Expected "-1.1" as the result, the result was: ' + findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ) );
     equal( findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, -Infinity, 0.18, -1.1, 12] ), -1.1, 'Expected "-Infinity" as the result, the result was: ' + findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, -Infinity, 0.18, -1.1, 12] ) );
 });
@@ -48,7 +50,8 @@ test( "findDistinctValues Test", function() {
     }
 
     // Check via shortcut function for readability
-    testShortcut( [1, 1, 2, 3, 5, 7], [1, 2, 3, 5, 7]);
+    // First test is taken from data/testdata.txt:
+    testShortcut( [1, 1, 2, 3, 5, 7], [1, 2, 3, 5, 7]); 
     testShortcut( ['apple', 'banana', 'orange', 'kiwi', 'pear', 'plum', 'strawberry'], ['apple', 'banana', 'orange', 'kiwi', 'pear', 'plum', 'strawberry']);    
     testShortcut( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, -Infinity, 0.18, -1.1, 12], [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, -Infinity ] );
     // Test that results don't depend on the order of elements in actual and expected arrays (we take into accout only that values must be distinct)
