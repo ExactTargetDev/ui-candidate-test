def4a29b165d84027d78cc1bdcdd6bf25864deb3
rostag <rostislav.siryk@gmail.com>
2013-10-21 16:07:53 +0300

    Implemented reverseString in pure JS and QUnit test for it.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,16 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+module( "Test Suite" );
+test( "reverseString Test", 7, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+    // Check the reverseString function works right
+    equal( reverseString( "123" ), "321", 'Expected "321" as the result, the result was: ' + reverseString( "123" ) );
+    equal( reverseString( "abcd" ), "dcba", 'Expected "dcba" as the result, the result was: ' + reverseString( "abcd" ) );
+    equal( reverseString( "1    z" ), "z    1", 'Expected "z    1" as the result, the result was: ' + reverseString( "1    z" ) );
+    equal( reverseString( " a#X -12 3" ), "3 21- X#a ", 'Expected "3 21- X#a " as the result, the result was: ' + reverseString( " a#X -12 3" ) );
+    equal( reverseString( "4, 2" ), "2 ,4", 'Expected "2 ,4" as the result, the result was: ' + reverseString( "4, 2" ) );
+    equal( reverseString( " 	 	" ), "	 	 ", 'Expected " 	 	" as the result, the result was: ' + reverseString( " 	 	" ) );
+});
