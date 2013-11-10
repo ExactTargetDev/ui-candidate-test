caf659b2fa7a009740605be801504531d028fb64
Stephen James <llama@interactivellama.com>
2013-11-09 19:26:16 -0500

    reverseString function - fixed typo

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -24,7 +24,7 @@ test( "Reverse a String", 2, function() {
 module( "JS and DOM tests" );
 test( "Find minimum value", 4, function() {
     // Verify the method exists
-    equal( typeof reverseString, 'function', 'Must contain a minimum value function' );
+    equal( typeof findMinValue, 'function', 'Must contain a minimum value function' );
 
     equal( findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ), '-1.1', 'Expected -1.1 as the result, the result was: ' + findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ) );
 
