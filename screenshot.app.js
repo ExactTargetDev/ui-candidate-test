8eb7fc353e209e9bc6e20208150775955dd0293f
Brett Timperman <brett.timperman@gmail.com>
2013-12-20 06:00:59 -0500

    implemented base functionality - pulse dashboard structure, complete help modal

--- screenshot.app.js
+++ screenshot.app.js
@@ -1,8 +1,8 @@
 var screenshot = angular.module('screenshot', ['ui.bootstrap']);
 
-screenshot.controller('MarketingHubController', ['$scope', '$http', function($scope, $http) {
+screenshot.controller('MarketingHubController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
 	$scope.dashboard = {};
-	$scope.help = {};
+	$scope.dashboardLoading = true;
 
 	var init = function() {
 		var now = moment();
@@ -12,10 +12,41 @@ screenshot.controller('MarketingHubController', ['$scope', '$http', function($sc
 	};
 
 	$http.get('data/dashboard.json').success(function(data) {
-		$scope.dashboard = data;
+		$scope.dashboard = _.map(data.pulse, function(item) { return _.extend(item, {selected: false}); });
+		$scope.dashboardLoading = false;
 	}).error(function(e) {
+		$scope.dashboardLoading = false;
 		console.log(e);
 	});
 
+	$scope.selectItem = function (item) {
+		_.each(_.without($scope.dashboard, item), function(existingItem) { existingItem.selected = false; });
+		item.selected = true;
+	};
+
+	$scope.openHelp = function () {
+		var modalInstance = $modal.open({
+			templateUrl: "modal_content.html", 
+			controller: 'ModalController',
+			resolve: {}
+		});
+	};
+
 	init();
+}]);
+
+screenshot.controller('ModalController', ['$scope', '$http', function($scope, $http) {
+	$scope.help = {};
+	$scope.helpLoading = true;
+
+	$http.get('data/help.json').success(function(data) {
+		$scope.help = data.help;
+		$scope.helpLoading = false;
+	}).error(function(e) {
+		$scope.$dismiss("ok");
+	});
+
+	$scope.ok = function() {
+		$scope.$dismiss("ok");
+	};
 }]);
