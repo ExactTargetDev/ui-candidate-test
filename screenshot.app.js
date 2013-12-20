120ec98e99cdb098fca17f324f39c5644115614e
Brett Timperman <brett.timperman@gmail.com>
2013-12-20 04:04:42 -0500

    http pull of json files and simple node server for testing

--- screenshot.app.js
+++ screenshot.app.js
@@ -1,12 +1,21 @@
 var screenshot = angular.module('screenshot', ['ui.bootstrap']);
 
-screenshot.controller('MarketingHubController', ['$scope', function($scope) {
+screenshot.controller('MarketingHubController', ['$scope', '$http', function($scope, $http) {
+	$scope.dashboard = {};
+	$scope.help = {};
+
 	var init = function() {
 		var now = moment();
 		var todaysDay = now.day();
 		$scope.today = new Date(now);
 		$scope.week = _.map(_.range(0,7), function(index) { return { date: new Date(now.day(index)), today: todaysDay == index }; });
 	};
+
+	$http.get('data/dashboard.json').success(function(data) {
+		$scope.dashboard = data;
+	}).error(function(e) {
+		console.log(e);
+	});
 
 	init();
 }]);
