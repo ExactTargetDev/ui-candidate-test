4cb2532ae678a0185d9a7acd65dc732e4e8cbce0
Austin Stoltzfus <astoltzf@gmail.com>
2014-01-14 21:58:40 -0500

    Remove duplicate fruits from array problem finished

--- jsanddom.js
+++ jsanddom.js
@@ -37,8 +37,13 @@
      // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
      // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
-     function removeFruits(fruits, fruitsToRemove) {
-         // FILL THIS IN
+     function removefruits(fruits, fruitstoremove) {
+         $(fruits).each(function(index, val){
+             if($.inarray(val, fruitstoremove, 0) !== -1) {
+                 fruits.splice(index, 1);
+             }
+         });
+         return fruits;
      }
 
      // Write a function to push either a simple value or an array of values onto a specified array.
