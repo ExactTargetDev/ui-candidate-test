3bf8e386a250f6b65ef31edd6a5c687db4e910c5
Austin Stoltzfus <astoltzf@gmail.com>
2014-01-16 18:33:07 -0500

    Layout finished - Help modal with JSON in progress

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+    var url = '/data/help.json';
+    // Import Help JSON
+    $.getJSON( url, function(data) {
+        $('.modal-content').html($.toJSON(data));
+    });
+
     $('.pulse-box').click(function() {
         if( !$(this).hasClass('active') ) {
 
@@ -6,5 +13,13 @@ $(document).ready(function() {
             $(this).siblings('.pulse-box').removeClass('active');
 
         }
+    });
+
+    $('.help-modal-outer').hide();
+    $('.open-modal').click(function() {
+        $('.help-modal-outer').show();
+    });
+    $('.close-modal').click(function() {
+        $('.help-modal-outer').hide();
     });
 });
