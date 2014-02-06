744dd0680163f66cf859d11cfde58fd4847dd025
Matt Williams <mwilliams@cik.com>
2014-02-06 11:46:10 -0500

    Added splitListStrUsingComma and isOnlyWhitespace functions, as well as jQuery-powered solutions for the pushOntoArray and removeFruits functions.

--- jsanddom.js
+++ jsanddom.js
@@ -48,7 +48,16 @@
     // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
     // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
     function removeFruits(fruits, fruitsToRemove) {
-        // FILL THIS IN
+        for (var i = 0; i < fruits.length; i++) {
+            if (window.jQuery) {
+                if ($.inArray(fruits[i], fruitsToRemove) !== -1) {
+                    // Is in array
+                    fruits.splice(i, 1);
+                }
+            } else {
+                // No jQuery
+            }
+        }
     }
 
     // Write a function to push either a simple value or an array of values onto a specified array.
@@ -56,12 +65,20 @@
     // If toPush is a simple value, it should be pushed onto array as an element.
     // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
     function pushOntoArray(array, toPush) {
-        // FILL THIS IN
+        if (window.jQuery) {
+            $.extend(1, array, toPush);
+        } else {
+            // No jQuery
+        }
+        return array;
     }
 
     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
     function splitListStrUsingComma(sourceStr) {
-        // FILL THIS IN
+        if (sourceStr === '')
+            return [];
+        else
+            return sourceStr.split(',');
     }
 
     // Write a function that will take any number of arguments and return their sum
@@ -71,7 +88,11 @@
 
     // Write a function that will return true if a specified string consists of only whitespace.
     function isOnlyWhitespace(sourceStr) {
-        // FILL THIS IN
+        if (/\S/.test(sourceStr)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     // write an example of a javascript closure
