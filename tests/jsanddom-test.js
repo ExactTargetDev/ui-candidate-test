d705cb62d24c0eb2ea4db8c50515fc34785cda2b
Matt Williams <mwilliams@cik.com>
2014-02-07 16:16:41 -0500

    Initial unit tests setup. Added new module to run and a skeleton of the tests that will need to be written/run. @todo: Add remaining unit test functions.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,44 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+(function () {
+    
+    module( 'JSandDOM Tests' );
+    test( 'Reverse the Given String', function () {
+        equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+        strictEqual( reverseString( 'Subscribers rock' ), 'kcor srebircsbuS', 'Expected "kcor srebircsbuS", got: ' + reverseString( 'string' ) );
+    });
+
+    test( 'Find Min Value in Array', function () {
+
+    });
+
+    test( 'Find Distinct Value in Array', function () {
+        
+    });
+
+    test( 'Do Fizz Buzz Test', function () {
+        
+    });
+
+    test( 'Remove Fruits Test', function () {
+        
+    });
+
+    test( 'Push Value Onto Array', function () {
+        
+    });
+
+    test( 'Split List String Using Comma Test', function () {
+        
+    });
+
+    test( 'Find Sum of Unlimited Arguments', function () {
+
+    });
+
+    test( 'String is Only Whitespace Test', function () {
+        
+    });
+
+});
