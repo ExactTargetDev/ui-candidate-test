1ca31ca8e4e587a98c46d1ae7f03f2d1aa88d086
Mike Grace <junkmailbonzai@gmail.com>
2014-02-12 12:23:02 -0500

    Created tests to write functions. Started writing most of the functions but need to help clean the house for moving. Committing before break.

--- jsanddom.js
+++ jsanddom.js
@@ -1,98 +1,155 @@
-     // Example unit test function
-     function divide( a, b ) {
-        // To see the test pass, uncomment the following line
-        //return a / b;
-     }
+/*jshint unused:false, devel:true */
+'use strict';
 
-     // Write a function that takes a single argument (a string) and returns the string reversed.
-     function reverseString(str) {
-         // FILL THIS IN
-     }
+// Example unit test function
+function divide( a, b ) {
+	// To see the test pass, uncomment the following line
+	return a / b;
+}
 
-     // Write a function that takes an array of numbers and returns the minimum value
-     function findMinValue(values) {
-         // FILL THIS IN
-     }
+// Write a function that takes a single argument (a string) and returns the string reversed.
+function reverseString(str) {
+	return str.split('').reverse().join('');
+}
 
-     // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
-     function findDistinctValues(values) {
-         // FILL THIS IN
-     }
+// Write a function that takes an array of numbers and returns the minimum value
+function findMinValue(values) {
+	var min = null;
+	var i;
 
-     // Write a function that logs the numbers from 1 to 100 to the console.
-     // For multiples of three print "Fizz" instead of the number.
-     // For multiples of five print "Buzz".
-     // For numbers which are multiples of both three and five print "FizzBuzz".
-     function doFizzBuzz() {
-         // FILL THIS IN
-     }
+	 for (i = 0; i < values.length; i++) {
+		if ( min === null || values[i] < min ) {
+			min = values[i];
+		}
+	}
+	return min;
+}
 
-     // You have a master array of strings, where each element is a fruit name.
-     // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
-     // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
-     // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
-     function removeFruits(fruits, fruitsToRemove) {
-         // FILL THIS IN
-     }
+// Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
+function findDistinctValues(values) {
+	var distinct = [];
+	var valuesIndex;
+	var distinctIndex;
+	var foundDistinct;
 
-     // Write a function to push either a simple value or an array of values onto a specified array.
-     // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
-     // If toPush is a simple value, it should be pushed onto array as an element.
-     // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
-     function pushOntoArray(array, toPush) {
-         // FILL THIS IN
-     }
+	// slow search algorithm but works for expected range of inputs
+	for (valuesIndex = 0; valuesIndex < values.length; valuesIndex++) {
+		foundDistinct = true;
+		for (distinctIndex = 0; distinctIndex < distinct.length; distinctIndex++) {
+			if( values[valuesIndex] === distinct[distinctIndex] ) {
+				foundDistinct = false;
+				break;
+			}
+		}
+		if ( foundDistinct === true ) {
+			distinct.push( values[valuesIndex] );
+		}
+	}
+	return distinct;
+}
 
-     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
-     function splitListStrUsingComma(sourceStr) {
-         // FILL THIS IN
-     }
+// Write a function that logs the numbers from 1 to 100 to the console.
+// For multiples of three print "Fizz" instead of the number.
+// For multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+function doFizzBuzz() {
+	var outputToConsole = [];
+	var min = 1;
+	var max = 100;
+	var i;
+	var tmpOutput;
 
-     // Write a function that will take any number of arguments and return their sum
-     function sum() {
-         // FILL THIS IN
-     }
+	for (i = min; i < max + 1; i++) {
+		tmpOutput = '';
+		if ( i % 3 === 0 ) {
+			tmpOutput = 'Fizz';
+		}
+		if ( i % 5 === 0 ) {
+			tmpOutput += 'Buzz';
+		}
+		if ( tmpOutput === '' ) {
+			tmpOutput = i;
+		}
+		outputToConsole.push( tmpOutput );
+	}
+	console.log( outputToConsole.join( '\n' ) );
+}
 
-     // Write a function that will return true if a specified string consists of only whitespace.
-     function isOnlyWhitespace(sourceStr) {
-         // FILL THIS IN
-     }
+// You have a master array of strings, where each element is a fruit name.
+// You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
+// For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
+// Write the function that will remove the values contained in fruitsToRemove from the array fruits.
+function removeFruits(fruits, fruitsToRemove) {
+	var removalIndex;
 
-     // write an example of a javascript closure
+	for (var i = 0; i < fruitsToRemove.length; i++) {
+		removalIndex = fruits.indexOf( fruitsToRemove[i] );
+		console.log( removalIndex );
+		if ( removalIndex !== -1 ) {
+			fruits.splice( removalIndex, 1 );
+		}
+	}
+	return fruits;
+}
 
-     // define a json object that represents a collection of people.
-     // each person should have the following properties
-     // - first name
-     // - last name
-     // - city
-     // - state
-     // - zip
-     // - a collection of phone numbers (home, work, mobile)
+// Write a function to push either a simple value or an array of values onto a specified array.
+// For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
+// If toPush is a simple value, it should be pushed onto array as an element.
+// If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
+function pushOntoArray(array, toPush) {
+	// FILL THIS IN
+}
+
+// Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
+function splitListStrUsingComma(sourceStr) {
+	// FILL THIS IN
+}
+
+// Write a function that will take any number of arguments and return their sum
+function sum() {
+	// FILL THIS IN
+}
+
+// Write a function that will return true if a specified string consists of only whitespace.
+function isOnlyWhitespace(sourceStr) {
+	// FILL THIS IN
+}
+
+// write an example of a javascript closure
+
+// define a json object that represents a collection of people.
+// each person should have the following properties
+// - first name
+// - last name
+// - city
+// - state
+// - zip
+// - a collection of phone numbers (home, work, mobile)
 
 
-     // Create a javascript object (DataTable) with the following:
-     // A private columns property (string array)
-     // A private rows property (string array)
-     // A public method addRows that adds an item to the rows array
-     // A public method addColumns that adds an item to the columns array
-     // A public method getData that returns the a json object representation of the DataTable
-     // Note: the row object should be a hash of the column name and row item value
-     // Example:
-     // .addColumns('column1', 'column2', 'column3');
-     // .addRow('value1A', 'value1B', 'value1C');
-     // .addRow('value2A', 'value2B', 'value2C');
+// Create a javascript object (DataTable) with the following:
+// A private columns property (string array)
+// A private rows property (string array)
+// A public method addRows that adds an item to the rows array
+// A public method addColumns that adds an item to the columns array
+// A public method getData that returns the a json object representation of the DataTable
+// Note: the row object should be a hash of the column name and row item value
+// Example:
+// .addColumns('column1', 'column2', 'column3');
+// .addRow('value1A', 'value1B', 'value1C');
+// .addRow('value2A', 'value2B', 'value2C');
 
-     // within div1, programatically create a
-     // SELECT element (with multiple items) and a button.
-     // when the button is clicked write out the name and value of the selected item to the console.
+// within div1, programatically create a
+// SELECT element (with multiple items) and a button.
+// when the button is clicked write out the name and value of the selected item to the console.
 
-     // Write 5 different jQuery selectors to retrieve the
-     // sample anchor in the markup below.
+// Write 5 different jQuery selectors to retrieve the
+// sample anchor in the markup below.
 
-     // Programatically create an array with 5 items.  Create a list item for each item in the array
-     // and add the list items to the unordered list with an id of "list1".
+// Programatically create an array with 5 items.  Create a list item for each item in the array
+// and add the list items to the unordered list with an id of "list1".
 
-     // Use javascript to add a list of checkboxes and 2 links
-     // to the div with an id of "foobar"
-     // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
-     // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
+// Use javascript to add a list of checkboxes and 2 links
+// to the div with an id of "foobar"
+// When the first link is clicked, all the checkboxes should be checked (i.e. check all)
+// When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
