39fa1458dcea2299a8c19b050692d05b66008c60
winonalakereid <rdbishop@gmail.com>
2014-03-09 23:04:52 -0400

    Wrote Minimum Value Unit Test

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -17,3 +17,10 @@ test("String Reverse Test", function(){
 	equal( typeof reverseString, 'function', 'Must contain reverseString function');
 	ok( reverseString("Reid"),"deiR", "Expected deiR as result, test result was: " + reverseString("Reid"));
 });
+
+module("Minimum Value Unit Test");
+test("Minimum Value Test", 2, function(){
+	equal( typeof findMinValue, 'function', 'Must contain findMinValue function');
+	ok(findMinValue(), 1, "Expected 1 as result, test result was:" + findMinValue());
+	
+});
