f124c60eca14c2c55b9035b8bb7d3bd0bc969bef
ehannum <ehannum@gmail.com>
2014-05-19 13:41:49 -0700

    reverseString working and tested. Also fixed indentation so there's going to be a lot more lines changed than are really used.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -3,11 +3,18 @@ Unit Test Example
 ********************************/
 module( "Example Unit Test" );
 test( "Example Test", 2, function() {
-    // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+  // Verify the method exists
+  equal( typeof divide, 'function', 'Must contain a divide function' );
 
-    // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+  // Make sure the result from the divide function is valid
+  ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+});
+test( "Reverseing a string", 2, function() {
+  // Verify the method exists
+  equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+  // Make sure the result from the divide function is valid
+  ok( reverseString( 'Banana' ), 'ananaB', 'Expected ananaB as the result, the result was: ' + divide( 'Banana' ) );
 });
 /********************************
 Please create your tests below...
