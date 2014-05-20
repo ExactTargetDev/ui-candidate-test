a9637affbef3eeddeaf5de1c48826083eb82fc11
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-20 12:01:37 -0700

    Added Show Option Element test

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -142,7 +142,15 @@ test( "JSON Data Table", 1, function() {
       + DataTable.getData() );            
 });
 
+module( "Show Option Element" );
+test( "Show Option Element", 1, function() {
 
+    $("body").append("<div id='div1'></div>");
+    createEls();
+
+    // Verify the method exists
+    equal( typeof showOption, 'function', 'Must contain a showOption function' );     
+});
 
 
 
