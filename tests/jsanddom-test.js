fd703c282a3a20b7bdf88dbcd2aa3511b8cbccad
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-19 18:04:23 -0700

    Changed spec function to be 'equal' rather than 'ok' so as to not test for a Boolean

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -7,7 +7,7 @@ test( "Example Test", 2, function() {
     equal( typeof divide, 'function', 'Must contain a divide function' ); 
 
     // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+    equal( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
 });
 /********************************
 Please create your tests below...
