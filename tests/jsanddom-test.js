47d42595a2e8e1c0fd2343540508bd6213d9302f
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-19 18:22:45 -0700

    Added tests for findDistincyValues function

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -30,3 +30,15 @@ test( "Find Minimum Value", 2, function() {
     // Make sure the result from the function is valid
     equal( findMinValue( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ), -1.1, 'Expected -1.1 as the result, the result was: ' + findMinValue([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]) );
 });
+
+module( "Find Distinct Values" );
+test( "Find Distinct Values", 3, function() {
+    // Verify the method exists
+    equal( typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function' ); 
+
+    // Make sure the result from the function is valid (numbers)
+    deepEqual( findDistinctValues( [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] ), [3, 5, 7, 20, 0.18, 01, -1.1, 12, Infinity], 'Expected [3, 5, 7, 20, 0.18, 1, -1.1, 12, Infinity] as the result, the result was: ' + findDistinctValues([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]) );
+
+    // Make sure the result from the function is valid (strings)
+    deepEqual( findDistinctValues( ['apple', 'banana', 'orange', 'banana', 'pear', 'apple', 'strawberry'] ), ['apple', 'banana', 'orange', 'pear', 'strawberry'], 'Expected ["apple", "banana", "orange", "pear", "strawberry"] as the result, the result was: ' + findDistinctValues(['apple', 'banana', 'orange', 'banana', 'pear', 'apple', 'strawberry']) );    
+});
