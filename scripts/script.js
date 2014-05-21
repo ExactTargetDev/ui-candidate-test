4002a273f110aa8ef2f5a48eb4703980f4d1d92a
Ben Stone <benjamin.v.stone@gmail.com>
2014-05-21 11:47:20 -0700

    Basic expansion of widgets achieved. Needs refining

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-script.js
+
+$('.widget').on('click', function(){
+
+  if($(this).hasClass('widget')){
+    $(this).addClass('expand').removeClass('widget');
+    $(this).css('top', '-=25');
+    $(this).css('left', '-=25');
+  }else{
+    $(this).removeClass('expand').addClass('widget');
+    $(this).css('top', '+=25');
+    $(this).css('left', '+=25');
+  }
+});
+
+
+
