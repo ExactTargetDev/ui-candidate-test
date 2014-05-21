e04ce26dc0456544e3329eae7c8d8b6fb08de3cd
ehannum <ehannum@gmail.com>
2014-05-20 21:46:49 -0700

    [WIP] navbar click and hover states added

--- src/screenshot.js
+++ src/screenshot.js
@@ -2,3 +2,7 @@ $('#boxes').on('click', '.box', function(e){
   $('#boxes').children().not($(this)).removeClass('medium');
   $(this).toggleClass('medium');
 });
+$('#sidebar').on('click', 'a', function(e){
+  $('#sidebar').find('a').not($(this)).removeClass('orange');
+  $(this).addClass('orange');
+});
