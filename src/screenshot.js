9d9d64c47782821b1587e6a9fa07d49b5efdde8e
ehannum <ehannum@gmail.com>
2014-05-20 22:08:48 -0700

    [WIP] Help button retrieves JSON object with help interface contents

--- src/screenshot.js
+++ src/screenshot.js
@@ -1,8 +1,28 @@
-$('#boxes').on('click', '.box', function(e){
+$('#boxes').on('click', '.box', function (e) {
   $('#boxes').children().not($(this)).removeClass('medium');
   $(this).toggleClass('medium');
 });
-$('#sidebar').on('click', 'a', function(e){
+
+$('#sidebar').on('click', 'a', function (e) {
   $('#sidebar').find('a').not($(this)).removeClass('orange');
   $(this).addClass('orange');
 });
+
+// This needs to be on a server to test.
+// python -m SimpleHTTPServer works fine
+// Running straight out of the html document doesn't
+
+$('.help').click(function(){
+  $.ajax({
+    method: 'GET',
+    dataType: 'JSON',
+    url: 'src/help.json',
+    success: function (data) {
+      parseHelp(data);
+    }
+  });
+});
+
+var parseHelp = function (data) {
+  // todo: this
+};
