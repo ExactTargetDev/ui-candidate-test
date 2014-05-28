7fe583a7757664ecc94cecee825bfbe96e0641a9
Allen Kim <allenkim67@gmail.com>
2014-05-28 14:14:29 -0700

    implemented doFizzBuzz

--- jsanddom.js
+++ jsanddom.js
@@ -36,8 +36,27 @@
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
-         // FILL THIS IN
+       var fizzBuzz = fizzBuzzHelper(100);
+       for( var i = 0; i < fizzBuzz.length; i++ ) {
+         console.log( fizzBuzz[i] );
+       }
      }
+
+    function fizzBuzzHelper(n) {
+      var fizzBuzz = [];
+      for ( var i = 1; i <= n; i++ ) {
+        if ( i % 15 === 0 ) {
+          fizzBuzz.push("FizzBuzz");
+        } else if ( i % 3 === 0 ) {
+          fizzBuzz.push("Fizz");
+        } else if ( i % 5 === 0 ) {
+          fizzBuzz.push("Buzz");
+        } else {
+          fizzBuzz.push(i);
+        }
+      }
+      return fizzBuzz;
+    }
 
      // You have a master array of strings, where each element is a fruit name.
      // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -23,3 +23,7 @@ test("findMinValue", function() {
 test("findDistinctValues", function() {
   deepEqual(findDistinctValues([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]), [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity], "Should return unique values");
 });
+
+test("fizzBuzzHelper", function() {
+  deepEqual(fizzBuzzHelper(15), [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"], "Should return 'Fizz' for multiples of 3, 'Buzz' for multiples of 5, and 'FizzBuzz' for multiples of both 3 and 5");
+});
