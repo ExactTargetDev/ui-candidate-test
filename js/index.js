36687ec2393debc84de5608bf2a892cec9693641
ecMode <echen.contact@gmail.com>
2014-06-01 02:05:04 -0400

    Adding js to expand medium panels

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 $(document).ready(function (){
+	$('#help').click(function (e){
+		e.preventDefault();
+
+	});
 
 
+	$('.panel').click(function (){
+		var panels = $('.panel').not($(this));
+
+		panels.each(function(i){
+			if ($(this).hasClass('medium-panel')){
+				$(this).removeClass('medium-panel');
+				$(this).find('.stats').removeClass('medium');
+			}
+		});
+
+		$(this).addClass('medium-panel');
+		$(this).find('.stats').addClass('medium');
+	});
 });
