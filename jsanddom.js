ccc0e02a639e12c46a24c30ea20fda32c0e35e12
bofrank <bofrank@Bolinass-MacBook-Air.local>
2014-06-12 02:28:50 -0700

    creating a list

--- jsanddom.js
+++ jsanddom.js
@@ -232,6 +232,10 @@
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
 
+    var myArray = ["Greenlake","Eastlake","Westlake","Lakeview","Hmm, maybe even Bellevue"];
+    for(var i=0;i<myArray.length;i++){
+        $("#list1").append("<li>"+myArray[i]+"</li>");
+    }
      // Use javascript to add a list of checkboxes and 2 links
      // to the div with an id of "foobar"
      // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
