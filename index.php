3a90dbd67f7be2f4d7f41eba2ffe22b3ec9a1b83
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-23 21:53:18 -0400

    starting rough mockup, added et-site.less

--- index.php
+++ index.php
@@ -9,10 +9,34 @@
 
     <div id="et-app">
         <div id="header">
-
+            <div class="logo pull-left">
+                <span class="logo-text">ExactTarget.</span>
+            </div>
+            <div class="account-menu pull-right">
+                <ul>
+                    <li class="welcome">Welcome John!</li>
+                    <li><a href="account">Account</a></li>
+                    <li><a href="help">Help</a></li>
+                    <li><a href="logout">Logout</a></li>
+                </ul>
+            </div>
         </div>
         <div id="content">
-
+            <div id="left-nav">
+                <ul>
+                    <li class="heading">Interactive Marketing Hub</li>
+                    <li>ExactTarget Email</li>
+                    <li>ExactTarget Mobile</li>
+                    <li>CoTweet Social</li>
+                    <li>ExactTarget Sites</li>
+                    <li class="spacer"><hr /></li>
+                    <li>Automation Studio</li>
+                    <li>Attendence Builder</li>
+                    <li>Audience Builder</li>
+                    <li class="spacer"></li>
+                    <li>Partner Applications</li>
+                </ul>
+            </div>
         </div>
     </div>
 
