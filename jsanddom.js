fabfca839c5a64d40d1bee6eede56de1a91ee22e
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-24 17:16:58 -0400

    reformatted files

--- jsanddom.js
+++ jsanddom.js
@@ -1,139 +1,137 @@
-     // Example unit test function
-     function divide( a, b ) {
-        // To see the test pass, uncomment the following line
-        //return a / b;
-     }
+// Example unit test function
+function divide (a, b) {
+    // To see the test pass, uncomment the following line
+    return a / b;
+}
 
-     // Write a function that takes a single argument (a string) and returns the string reversed.
-     function reverseString(str) {
-         var len = str.length - 1,
-             result = '';
-         while(len >= 0) {
-             result += str[len--]
-         }
-         return result;
-     }
+// Write a function that takes a single argument (a string) and returns the string reversed.
+function reverseString (str) {
+    var len = str.length - 1, result = '';
+    while (len >= 0) {
+        result += str[len--]
+    }
+    return result;
+}
 
-     // Write a function that takes an array of numbers and returns the minimum value
-     function findMinValue(values) {
-         //You may use any JavaScript and/or CSS library you wish, or none at all.
-         return _.min(values);
-     }
+// Write a function that takes an array of numbers and returns the minimum value
+function findMinValue (values) {
+    //You may use any JavaScript and/or CSS library you wish, or none at all.
+    return _.min(values);
+}
 
-     // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
-     function findDistinctValues(values) {
-         return _.uniq(values);
-     }
+// Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
+function findDistinctValues (values) {
+    return _.uniq(values);
+}
 
-     // Write a function that logs the numbers from 1 to 100 to the console.
-     // For multiples of three print "Fizz" instead of the number.
-     // For multiples of five print "Buzz".
-     // For numbers which are multiples of both three and five print "FizzBuzz".
-     function doFizzBuzz() {
-         console.group('doFizzBuzz() Test');
-         var ct = 1,
-             output;
-         while(ct <= 100) {
-             output = ct.toString() + ' ';
+// Write a function that logs the numbers from 1 to 100 to the console.
+// For multiples of three print "Fizz" instead of the number.
+// For multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+function doFizzBuzz () {
+    console.group('doFizzBuzz() Test');
+    var ct = 1, output;
+    while (ct <= 100) {
+        output = ct.toString() + ' ';
 
-             if(ct % 3 === 0) {
-                 output += 'Fizz';
-             }
+        if (ct % 3 === 0) {
+            output += 'Fizz';
+        }
 
-             if(ct % 5 === 0) {
-                 output += 'Buzz';
-             }
-             console.log(output)
+        if (ct % 5 === 0) {
+            output += 'Buzz';
+        }
+        console.log(output);
 
-             ct++;
-         }
+        ct++;
+    }
 
-         console.groupEnd();
-     }
+    console.groupEnd();
+}
 
-     // You have a master array of strings, where each element is a fruit name.
-     // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
-     // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
-     // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
-     function removeFruits(fruits, fruitsToRemove) {
-         return _.filter(fruits, function(fruit) {
-             return fruitsToRemove.indexOf(fruit) === -1;
-         });
-     }
+// You have a master array of strings, where each element is a fruit name.
+// You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
+// For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
+// Write the function that will remove the values contained in fruitsToRemove from the array fruits.
+function removeFruits (fruits, fruitsToRemove) {
+    return _.filter(fruits, function(fruit) {
+        return fruitsToRemove.indexOf(fruit) === -1;
+    });
+}
 
-     // Write a function to push either a simple value or an array of values onto a specified array.
-     // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
-     // If toPush is a simple value, it should be pushed onto array as an element.
-     // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
-     function pushOntoArray(array, toPush) {
-         if (_.isArray(toPush)) {
-             array = array.concat(toPush);
-         } else {
-             array.push(toPush);
-         }
-         return array;
-     }
+// Write a function to push either a simple value or an array of values onto a specified array.
+// For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
+// If toPush is a simple value, it should be pushed onto array as an element.
+// If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
+function pushOntoArray (array, toPush) {
+    if (_.isArray(toPush)) {
+        array = array.concat(toPush);
+    } else {
+        array.push(toPush);
+    }
+    return array;
+}
 
-     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
-     function splitListStrUsingComma(sourceStr) {
-         var result = sourceStr.split(',');
-         if (result.length === 1 && result[0] === '') {
-             result = [];
-         }
-         return result;
-     }
+// Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
+function splitListStrUsingComma (sourceStr) {
+    var result = sourceStr.split(',');
+    if (result.length === 1 && result[0] === '') {
+        result = [];
+    }
+    return result;
+}
 
-     // Write a function that will take any number of arguments and return their sum
-     function sum() {
-         return _.reduce(arguments, function(memo, num) {
-             return memo + num;
-         }, 0);
-     }
+// Write a function that will take any number of arguments and return their sum
+function sum () {
+    return _.reduce(arguments, function(memo, num) {
+        return memo + num;
+    }, 0);
+}
 
-     // Write a function that will return true if a specified string consists of only whitespace.
-     function isOnlyWhitespace(sourceStr) {
-         var result = _.filter(sourceStr, function(letter) {
-             return letter !== ' '
-         });
+// Write a function that will return true if a specified string consists of only whitespace.
+function isOnlyWhitespace (sourceStr) {
+    var result = _.filter(sourceStr, function(letter) {
+        return letter !== ' '
+    });
 
-         return (result.length === 0)
-     }
+    return (result.length === 0)
+}
 
-     // write an example of a javascript closure
+// write an example of a javascript closure
 
-     // define a json object that represents a collection of people.
-     // each person should have the following properties
-     // - first name
-     // - last name
-     // - city
-     // - state
-     // - zip
-     // - a collection of phone numbers (home, work, mobile)
+// define a json object that represents a collection of people.
+// each person should have the following properties
+// - first name
+// - last name
+// - city
+// - state
+// - zip
+// - a collection of phone numbers (home, work, mobile)
 
 
-     // Create a javascript object (DataTable) with the following:
-     // A private columns property (string array)
-     // A private rows property (string array)
-     // A public method addRows that adds an item to the rows array
-     // A public method addColumns that adds an item to the columns array
-     // A public method getData that returns the a json object representation of the DataTable
-     // Note: the row object should be a hash of the column name and row item value
-     // Example:
-     // .addColumns('column1', 'column2', 'column3');
-     // .addRow('value1A', 'value1B', 'value1C');
-     // .addRow('value2A', 'value2B', 'value2C');
+// Create a javascript object (DataTable) with the following:
+// A private columns property (string array)
+// A private rows property (string array)
+// A public method addRows that adds an item to the rows array
+// A public method addColumns that adds an item to the columns array
+// A public method getData that returns the a json object representation of the DataTable
+// Note: the row object should be a hash of the column name and row item value
+// Example:
+// .addColumns('column1', 'column2', 'column3');
+// .addRow('value1A', 'value1B', 'value1C');
+// .addRow('value2A', 'value2B', 'value2C');
 
-     // within div1, programatically create a
-     // SELECT element (with multiple items) and a button.
-     // when the button is clicked write out the name and value of the selected item to the console.
+// within div1, programatically create a
+// SELECT element (with multiple items) and a button.
+// when the button is clicked write out the name and value of the selected item to the console.
 
-     // Write 5 different jQuery selectors to retrieve the
-     // sample anchor in the markup below.
+// Write 5 different jQuery selectors to retrieve the
+// sample anchor in the markup below.
 
-     // Programatically create an array with 5 items.  Create a list item for each item in the array
-     // and add the list items to the unordered list with an id of "list1".
+// Programatically create an array with 5 items.  Create a list item for each item in the array
+// and add the list items to the unordered list with an id of "list1".
 
-     // Use javascript to add a list of checkboxes and 2 links
-     // to the div with an id of "foobar"
-     // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
-     // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
+// Use javascript to add a list of checkboxes and 2 links
+// to the div with an id of "foobar"
+// When the first link is clicked, all the checkboxes should be checked (i.e. check all)
+// When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
