7b1a9640d6c8fb1b9a9a81a14d3fe68c88280027
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-24 14:47:19 -0400

    fixed initial test, tricksy hobbitses

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,13 +1,14 @@
 /********************************
 Unit Test Example
 ********************************/
-module( "Example Unit Test" );
-test( "Example Test", 2, function() {
+module("Travis Hubbard's Testathon");
+
+test( "divide()", 2, function(assert) {
     // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+    assert.equal(typeof divide, 'function', 'Must contain a divide function');
 
     // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+    assert.ok(divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide(4, 2));
 });
 /********************************
 Please create your tests below...
