7cdf6f9714c7c2fe4d63ea895af617dc00298d22
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-24 16:06:11 -0400

    initial added jsanddom tests that fail because TDD

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -13,3 +13,76 @@ test( "divide()", 2, function(assert) {
 /********************************
 Please create your tests below...
 ********************************/
+test( "reverseString()", 2, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof reverseString, 'function', 'Must contain a reverseString function');
+
+    testVal = 'AbcDeFg';
+    expectedVal = 'gFeDcbA';
+    result = reverseString(testVal);
+    // Make sure the result from the divide function is valid
+    assert.equal(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+});
+
+test( "findMinValue()", 2, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof findMinValue, 'function', 'Must contain a findMinValue function');
+
+    testVal = [8, 4, 165.3, 9, 2];
+    expectedVal = 2;
+    result = findMinValue(testVal);
+    // Make sure the result from the divide function is valid
+    assert.equal(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+});
+
+
+test( "findDistinctValues()", 2, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function');
+
+    testVal = [1, 2, 4, 3, 2, 1];
+    expectedVal = [1, 2, 4, 3];
+    result = findDistinctValues(testVal);
+    // Make sure the result from the divide function is valid
+    assert.deepEqual(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+});
+
+test( "doFizzBuzz()", 1, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof reverseString, 'function', 'Must contain a doFizzBuzz function');
+
+    doFizzBuzz();
+});
+
+
+test( "removeFruits()", 2, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof findDistinctValues, 'function', 'Must contain a removeFruits function');
+
+    testVal = ['Boysenberry', 'Dragonfruit', 'Huckleberry', 'Lychee', 'Purple Mangosteen'];
+    var removeVals = ['Lychee', 'Purple Mangosteen'];
+    expectedVal = ['Boysenberry', 'Dragonfruit', 'Huckleberry'];
+    result = removeFruits(testVal, removeVals);
+    // Make sure the result from the divide function is valid
+    assert.deepEqual(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+});
+
+test( "pushOntoArray()", 3, function(assert) {
+    // Verify the method exists
+    assert.equal(typeof reverseString, 'function', 'Must contain a pushOntoArray function');
+
+    var initialArr = ['Ingress', 'Captain', 'Potato', 4],
+        toPush = 'FAJITAS';
+    expectedVal = ['Ingress', 'Captain', 'Potato', 4, 'FAJITAS'];
+    result = pushOntoArray(initialArr, toPush);
+    // Make sure the result from the divide function is valid
+    assert.deepEqual(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+
+    initialArr = ['Ingress', 'Captain', 'Potato', 4],
+        toPush = ['FAJITAS', 'salsa', 'SunKing Osiris'];
+    expectedVal = ['Ingress', 'Captain', 'Potato', 4, 'FAJITAS', 'salsa', 'SunKing Osiris'];
+    result = pushOntoArray(initialArr, toPush);
+    // Make sure the result from the divide function is valid
+    assert.deepEqual(result, expectedVal, 'Expected ' + expectedVal + ' as the result, the result was: ' + result);
+
+});
