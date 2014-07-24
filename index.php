b0d8b5bc9bf6ad62fdf5620395471f5d679df781
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-23 20:26:55 -0400

    inital libs, index, css file added

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/et-site.css">
+    <script type="text/javascript" src="libs/jquery.js"></script>
+    <script type="text/javascript" src="libs/underscore.js"></script>
+    <script type="text/javascript" src="libs/backbone.js"></script>
+</head>
+<body>
+
+    <div id="et-app">
+        <div id="header">
+
+        </div>
+        <div id="content">
+
+        </div>
+    </div>
+
+</body>
+</html>
