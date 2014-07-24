313b19c2a55c23ac2a431d6e0106020a1d5f5f37
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-23 22:44:38 -0400

    added inital art assets, spritesheet, less driving css generation, and temporary dev_ folders to be deleted later after its gone to prod and we forgot about them

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="css/et-site.css">
+    <link rel="stylesheet" type="text/css" href="css/et-spritesheet.css">
     <script type="text/javascript" src="libs/jquery.js"></script>
     <script type="text/javascript" src="libs/underscore.js"></script>
     <script type="text/javascript" src="libs/backbone.js"></script>
@@ -24,17 +25,46 @@
         <div id="content">
             <div id="left-nav">
                 <ul>
-                    <li class="heading">Interactive Marketing Hub</li>
-                    <li>ExactTarget Email</li>
-                    <li>ExactTarget Mobile</li>
-                    <li>CoTweet Social</li>
-                    <li>ExactTarget Sites</li>
+                    <li class="heading">
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-hub"></span>
+                        <span class="nav-text">Interactive Marketing Hub</span>
+                    </li>
                     <li class="spacer"><hr /></li>
-                    <li>Automation Studio</li>
-                    <li>Attendence Builder</li>
-                    <li>Audience Builder</li>
-                    <li class="spacer"></li>
-                    <li>Partner Applications</li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-email"></span>
+                        <span class="nav-text">ExactTarget Email</span>
+                    </li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-mobile"></span>
+                        <span class="nav-text">ExactTarget Mobile</span>
+                    </li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-cotweet"></span>
+                        <span class="nav-text">CoTweet Social</span>
+                    </li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-sites"></span>
+                        <span class="nav-text">ExactTarget Sites</span>
+                    </li>
+                    <li class="spacer"><hr /></li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-automation"></span>
+                        <span class="nav-text">Automation Studio</span>
+                    </li>
+
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-audience"></span>
+                        <span class="nav-text">Audience Builder</span>
+                    </li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-report"></span>
+                        <span class="nav-text">Report Builder</span>
+                    </li>
+                    <li class="spacer"><hr /></li>
+                    <li>
+                        <span class="sprite-et-spritesheet et-spritesheet-icon-partner"></span>
+                        <span class="nav-text">Partner Applications</span>
+                    </li>
                 </ul>
             </div>
         </div>
