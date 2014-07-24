fbfa9d6a34b2ed317e50df65986f9e35d1179506
Travis Hubbard <t.shubbard2@gmail.com>
2014-07-24 13:57:44 -0400

    reformatted code to PSR2

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="css/et-site.css">
@@ -9,84 +11,83 @@
 </head>
 <body>
 
-    <div id="et-app">
-        <div id="header">
-            <div class="logo pull-left">
-                <span class="logo-text">ExactTarget.</span>
-            </div>
-            <div class="account-menu pull-right">
-                <ul>
-                    <li class="welcome">Welcome John!</li>
-                    <li><a href="account">Account</a></li>
-                    <li><a href="help">Help</a></li>
-                    <li><a href="logout">Logout</a></li>
-                </ul>
-            </div>
+<div id="et-app">
+    <div id="header">
+        <div class="logo pull-left">
+            <span class="logo-text">ExactTarget.</span>
         </div>
-
-        <div id="content">
-
-            <div id="left-nav">
-                <div class="heading">
-                    <span class="sprite-et-spritesheet et-spritesheet-icon-hub icon-hub"></span>
-                    <span class="nav-text hub-text">Interactive<br />Marketing Hub</span>
-                </div>
-                <ul>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-email"></span>
-                        <span class="nav-text">ExactTarget Email</span>
-                    </li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-mobile"></span>
-                        <span class="nav-text">ExactTarget Mobile</span>
-                    </li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-cotweet"></span>
-                        <span class="nav-text">CoTweet Social</span>
-                    </li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-sites"></span>
-                        <span class="nav-text">ExactTarget Sites</span>
-                    </li>
-                    <li class="spacer"><hr /></li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-automation"></span>
-                        <span class="nav-text">Automation Studio</span>
-                    </li>
-
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-audience"></span>
-                        <span class="nav-text">Audience Builder</span>
-                    </li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-report"></span>
-                        <span class="nav-text">Report Builder</span>
-                    </li>
-                    <li class="spacer"><hr /></li>
-                    <li>
-                        <span class="sprite-et-spritesheet et-spritesheet-icon-partner"></span>
-                        <span class="nav-text">Partner Applications</span>
-                    </li>
-                </ul>
-
-            </div>
-
-            <div id="main">
-                <div id="calendar-container">
-                    <div id="calendar">
-                        <img src="images/tmpCalendarPH.jpg" alt="temporary placeholder for the calendar widget"/>
-                    </div>
-                </div>
-                <div id="pulse-container">
-                    <span class="sprite-et-spritesheet et-spritesheet-pulse-title pulse-title"></span>
-                    &nbsp;
-                </div>
-            </div>
-
-
+        <div class="account-menu pull-right">
+            <ul>
+                <li class="welcome">Welcome John!</li>
+                <li><a href="account">Account</a></li>
+                <li><a href="help">Help</a></li>
+                <li><a href="logout">Logout</a></li>
+            </ul>
         </div>
-
     </div>
+
+    <div id="content">
+        <div id="left-nav">
+            <div class="heading">
+                <span class="sprite-et-spritesheet et-spritesheet-icon-hub icon-hub"></span>
+                <span class="nav-text hub-text">Interactive<br/>Marketing Hub</span>
+            </div>
+            <ul>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-email"></span>
+                    <span class="nav-text">ExactTarget Email</span>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-mobile"></span>
+                    <span class="nav-text">ExactTarget Mobile</span>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-cotweet"></span>
+                    <span class="nav-text">CoTweet Social</span>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-sites"></span>
+                    <span class="nav-text">ExactTarget Sites</span>
+                </li>
+                <li class="spacer">
+                    <hr/>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-automation"></span>
+                    <span class="nav-text">Automation Studio</span>
+                </li>
+
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-audience"></span>
+                    <span class="nav-text">Audience Builder</span>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-report"></span>
+                    <span class="nav-text">Report Builder</span>
+                </li>
+                <li class="spacer">
+                    <hr/>
+                </li>
+                <li>
+                    <span class="sprite-et-spritesheet et-spritesheet-icon-partner"></span>
+                    <span class="nav-text">Partner Applications</span>
+                </li>
+            </ul>
+        </div>
+
+        <div id="main">
+            <div id="calendar-container">
+                <div id="calendar">
+                    <img src="images/tmpCalendarPH.jpg" alt="temporary placeholder for the calendar widget"/>
+                </div>
+            </div>
+            <div id="pulse-container">
+                <span class="sprite-et-spritesheet et-spritesheet-pulse-title pulse-title"></span>
+                &nbsp;
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
