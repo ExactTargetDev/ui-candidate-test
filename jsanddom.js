6323300ab99a45a8d0ab9fd500fcd13674dbb922
Sam Kligerman <sam.kligerman@gmail.com>
2014-09-02 19:05:02 -0400

    adding jQuery and 5 jQuery selectors library

--- jsanddom.js
+++ jsanddom.js
@@ -177,6 +177,11 @@
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
+     $('a.link');
+     $('#fizz > a.link');
+     $("a:contains('sample')");
+     $("a.link[href|='#']").text();
+     $("#fizz a:first-child");
 
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
