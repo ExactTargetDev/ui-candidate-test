ee421784804fcf3b1b50c8259e469043dab9cc57
John T. Ryan <john@jotryan.com>
2014-09-09 16:29:46 -0400

    finish jsanddom.html, jsanddom.js, jsanddom-test.js

--- js/jsanddom.js
+++ js/jsanddom.js
@@ -67,7 +67,6 @@
                 }
             }
          }
-         console.log(fruits)
          return fruits;
      }
 
@@ -157,85 +156,88 @@
      // .addRow('value1A', 'value1B', 'value1C');
      // .addRow('value2A', 'value2B', 'value2C');
      //
-     // var DataTable = {
-     //    priv columns: [''],
-     //    priv rows: [''],
+     var DataTable = {
 
-     //    addRows: function() {
 
-     //    },
+        function addRows() {
 
-     //    addColumns: function() {
+        },
 
-     //    }
+        function addColumns() {
 
-     //    getData: fucntion() {
+        }
 
-     //    }
-     // }
+        function getData() {
+
+        }
+
+        var rows =  [''];
+        var columns = [''];
+     }
 
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
 $(document).ready(function(){
-     // .div1
-     //
+
+
 var myDiv = document.getElementById("div1");
         //Create array of options to be added
-    var cars = ["Camaro","Beetle","Prius","Charger", "Shelby Mustang"];
-        //Create and append select list
-    var selectList = document.createElement("select");
-    selectList.setAttribute("id", "mySelect");
-    selectList.setAttribute("name", "mySelect");
-    myDiv.appendChild(selectList);
-        //Create and append the first option
-        var option = document.createElement("option");
-        option.setAttribute("value", "");
-        option.text = '- Select -';
-        selectList.appendChild(option);
-        // add cars to select list
-    for (var i = 0; i < cars.length; i++) {
-        var option = document.createElement("option");
-        option.setAttribute("value", cars[i]);
-        option.text = cars[i];
-        selectList.appendChild(option);
-    }
-        // create button
-    var btn = document.createElement("input");
-    btn.setAttribute('id','callfunc');
-    btn.setAttribute('type','button');
-    btn.setAttribute('value','Submit');
-    myDiv.appendChild(btn);
-    $('#callfunc').on('click', function(e){
-        e.preventDefault();
-        console.log(document.getElementById('mySelect').value);
-    })
+var cars = ["Camaro","Beetle","Prius","Charger", "Shelby Mustang"];
+    //Create and append select list
+var selectList = document.createElement("select");
+selectList.setAttribute("id", "mySelect");
+selectList.setAttribute("name", "mySelect");
 
+myDiv.appendChild(selectList);
+    //Create and append the first option
+    var option = document.createElement("option");
+    option.setAttribute("value", "");
+    option.text = '- Select -';
+    selectList.appendChild(option);
+    // add cars to select list
+for (var i = 0; i < cars.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", cars[i]);
+    option.text = cars[i];
+    selectList.appendChild(option);
+}
+    // create button
+var btn = document.createElement("input");
+btn.setAttribute('id','callfunc');
+btn.setAttribute('type','button');
+btn.setAttribute('value','Submit');
+myDiv.appendChild(btn);
+$('#callfunc').on('click', function(e){
+    e.preventDefault();
+    console.log(document.getElementById('mySelect').value);
+});
 
-
+/**********************************************/
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
-
     $(".link");
     $("#fizz.buzz");
     $("#foo.bar #fizz.buzz");
     $(".link a");
     $("#fizz.buzz .link");
 
+/**********************************************/
+
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
 
-    // var idList = document.getElementById("list1");
+    var listArray = ['Bread', 'Milk', 'Cheese', 'Sardines', 'Beer']; //create array of list items
+    var listEl = document.getElementById("list1");
+    //Create and append ul list
+    for (i = 0; i < listArray.length; i++) {
+        var litem = document.createElement("li");
+        litem.innerHTML = listArray[i];
+        listEl.appendChild(litem);
+    }
 
-    // var listArray = ['Bread', 'Milk', 'Cheese', 'Sardines', 'Beer']; //create array of list items
-
-//Create and append ul list
-    // for (i = 0; i < listArray.length; i++) {
-    //     var ulist = document.createElement("li");
-    //     idList.appendChild(ulist);
-    //     ulist.innerHTML = listArray[i];
-    // }
+/**********************************************/
 
 
      // Use javascript to add a list of checkboxes and 2 links
@@ -243,10 +245,7 @@ var myDiv = document.getElementById("div1");
      // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
      // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
 
-
-
 //Create and add checkboxes
-
     var chk= document.getElementById("foobar");
 //Create array of chk box names to be added
     var array = ["red", "green", "yellow", "blue"];
@@ -267,32 +266,32 @@ var myDiv = document.getElementById("div1");
        newform.appendChild(label);
     }
 
-    var addanchor = document.createElement('a');
-    addanchor.setAttribute('href', 'enableChk()');
-    addanchor.name = 'enable';
-    newform.appendChild(addanchor);
-    addanchor.innerHTML="Enable";
+    var addbutton = document.createElement('button');
+    addbutton.setAttribute('id', 'enable');
+    addbutton.name = 'enable';
+    newform.appendChild(addbutton);
+    addbutton.innerHTML="Enable";
 
-    addanchor = document.createElement('a');
-    addanchor.setAttribute('href', 'disableChk()');
-    addanchor.name = 'disable';
-    newform.appendChild(addanchor);
-    addanchor.innerHTML="Disable";
+    addbutton = document.createElement('button');
+    addbutton.setAttribute('id', 'disable');
+    addbutton.name = 'disable';
+    newform.appendChild(addbutton);
+    addbutton.innerHTML="Disable";
 
-    function enableChk() {
+    $("#enable").click(function(e) {
+        e.preventDefault();
         var selectCheck = document.getElementsByTagName('input');
         for (var i = 0; i < selectCheck.length; i++ ) {
             selectCheck[i].checked = true;
         }
-    }
+    });
 
-    function disableChk(){
+    $("#disable").click(function(e){
         var selectCheck = document.getElementsByTagName('input');
         for (var i = 0; i < selectCheck.length; i++ ) {
             selectCheck[i].checked = false;
         }
-    }
-
+    });
 
 });
 // END $(document).ready()
