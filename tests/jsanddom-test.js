5fd53d3626ceaca4490db8445db11e921e44ed9a
John T. Ryan <john@jotryan.com>
2014-09-09 10:22:16 -0400

    Initial commit

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -4,7 +4,7 @@ Unit Test Example
 module( "Example Unit Test" );
 test( "Example Test", 2, function() {
     // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+    equal( typeof divide, 'function', 'Must contain a divide function' );
 
     // Make sure the result from the divide function is valid
     ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
@@ -12,3 +12,12 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+module( "Example Unit Test" );
+test( "Example Test", 2, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+    // Make sure the result from the divide function is valid
+    ok( reverseString( "Hello Exact Target" ), 'tegraT tcaxE olleH', 'Expected 2 as the result, the result was: ' + reverseString( "Hello Exact Target") );
+});
+
