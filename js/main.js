22ef536a22367df9fe34d20a94647fca3733d21b
Lawrence Moore <lawrence2978@hotmail.com>
2014-11-27 14:52:58 -0500

    Worked on Pulse section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function(){
 
+
+    //Click function for menue
     $('#menu-list li').click(function(){
 
         $('#menu-list li').each(function(){
@@ -10,6 +12,32 @@ $(function(){
         $(this).addClass('active');
     });
 
+    //Spaces data boxes
+    $('.data-box').each(function( index, value ){
+        $(this).css({
+            left: [index] * 170
+        })
+    });
 
+    //Makes databoxes change size
+    $('.data-box').click(function(){
 
+        $('.data-box').each(function(){
+            $(this).animate({
+                width: 160,
+                height: 90
+            });
+        });
+        $(this).addClass('active');
+        $(this).animate({
+            width: 278,
+            height: 164
+        });
+    });
+
+    //Centers each data-info element
+    $('.data-info').each(function( index, value ){
+       var infoWidth =  $(this).width();
+        $(this).css('margin-left', '-=' + infoWidth / 2)
+    });
 });
