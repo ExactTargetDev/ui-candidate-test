75f35a685833d8843aa64bb18344e1ef40124d13
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2014-11-29 19:19:40 -0500

    pushOntoArray function
    JS task pushOntoArray completed and commented.

--- jsanddom.js
+++ jsanddom.js
@@ -74,12 +74,21 @@ $(document).ready( function(){
         }
     }
 
-    // Write a function to push either a simple value or an array of values onto a specified array.
-    // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
-    // If toPush is a simple value, it should be pushed onto array as an element.
-    // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
+    // Write a function to push either a value or an array of values onto a specified array.
+    // For the purpose of the exercise, we will call the target array "array" and the stuff to push onto it (either a value or array) "toPush".
+    // If "toPush" is a value, it should be pushed onto "array" as an element.
+    // If "toPush" is an array, all of its elements should be pushed onto "array". Your solution should modify "array" (ie. not return a new array).
     function pushOntoArray(array, toPush) {
-        // FILL THIS IN
+        //Check if "toPush" is an array or not
+        if(typeof toPush === "object") {
+            //if it is an array, then add each item of the array to "array"
+            for ( var i = 0; i < toPush.length; i++ ) {
+                array.push(toPush[i]);
+            }
+        } else {
+            //Otherwise just add it on its own
+            array.push(toPush);
+        }
     }
 
     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
