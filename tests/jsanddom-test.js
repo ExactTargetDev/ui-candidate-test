af7914e13c9f582296018328c1990f3bb2101b17
Eric Willhoit <iowillhoit@gmail.com>
2015-01-03 13:31:14 -0600

    Adds reverseString and Bower
    Adds 'esrever' library
    Adds tests for reverseString function
    Adds more files to .gitigmore

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -7,8 +7,24 @@ test( "Example Test", 2, function() {
     equal( typeof divide, 'function', 'Must contain a divide function' );
 
     // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+    strictEqual( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
 });
 /********************************
 Please create your tests below...
 ********************************/
+
+module( "Reverse String" );
+test( "Reverse String", 4, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+    // Make sure the result from the divide function is valid
+    strictEqual( reverseString( "doge" ), "egod", 'Expected "egod" as the result, the result was: ' + reverseString( "doge" ) );
+
+    // Ensure astral symbols reverse properly
+    strictEqual( reverseString( "foo 𝌆 bar" ), "rab 𝌆 oof", 'Expected "rab 𝌆 oof" as the result, the result was: ' + reverseString( "foo 𝌆 bar" ) );
+
+    // Ensure that combining marks reverse perperly
+    strictEqual( reverseString( "mañana mañana" ), "anañam anañam", 'Expected "anañam anañam" as the result, the result was: ' + reverseString( "mañana mañana" ) );
+
+});
