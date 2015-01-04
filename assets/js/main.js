3ab2c708d1ce9dab4d0ce5a684f476a398d11720
Eric Willhoit <iowillhoit@gmail.com>
2015-01-04 17:41:26 -0600

    Adds javascript functionality
    Pulse modules now increase and decrease on click, hoping to add subtle animation.
    Help navbar button opens Help modal using AJAX and handlebars templating.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
 $(function() {
+    /***************************
+    Toggle Pulse Modules
+    ***************************/
+    $(".pulse-module").on('click', function(){
+        if ( !$(this).hasClass("active") ) {
+            $(".active").removeClass('active');
+            $(this).addClass("active");
+        }
+    });
 
+    /***************************
+    Load Help Modal
+    ***************************/
+    $("#help").on('click', function(){
+         $.ajax({
+            url: "data/help.json",
+            dataType: "json",
+            success: function(data) {
+                var helpModalTemplate = $("#help-modal-template").html(),
+                    helpModalTemplateCompiled = Handlebars.compile(helpModalTemplate),
+                    helpModalHtml = helpModalTemplateCompiled(data);
+
+                $('#helpModalHolder').html(helpModalHtml);
+                $('#helpModal').modal();
+            }
+        });
+    })
 });
