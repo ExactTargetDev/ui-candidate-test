d4a8d6aa285b30fccea3036e3c8a86018175e1f3
Jonathan Whitney <jwhitney@exacttarget.com>
2015-01-08 16:37:52 -0500

    added findMinValue function and unit testing

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -24,3 +24,14 @@ test( "Reverse String Test", 4, function() {
     ok( reverseString( 'a' ), 'a', 'Expected a as the result, the result was: ' + reverseString( 'a' ) );
     ok( reverseString( 'abc123' ), '321cba', 'Expected 321cba as the result, the result was: ' + reverseString( 'abc123' ) );
 });
+
+// findMinValue
+module( "findMinValue Unit Test" );
+test( "Array Minimum Value Unit Test", 2, function() {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' ); 
+
+    // Make sure the result from the reverseString function is valid
+    var testArray = [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12];
+    ok( findMinValue( testArray ), -1.1, 'Expected -1.1 as the result, the result was: ' + findMinValue( testArray ) );
+});
