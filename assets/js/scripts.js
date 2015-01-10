89497e4c8ba652813a61e945cbbde7723c1ea945
Jonathan Whitney <jwhitney@exacttarget.com>
2015-01-10 11:38:28 -0500

    style fixes/additions, added functionality to programatically populate calendar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
+var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+	months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
 (function($) {
 	$(document).ready(function() {
+		// Get app launcher data
 		$.get('../data/applauncher.json', function(data) {
 			if (data) {
 				var templateScriptPrimary = $('#templateApps').html(),
@@ -12,6 +16,32 @@
 			}
 		});
 
+		// Get calendar data
+		var today = new Date(),
+			month = months[today.getMonth()],
+			year = today.getFullYear(),
+			week = { "dates": [] };
+
+		for (var i = -3; i <= 3; i++) {
+			var reset = new Date();
+				date = new Date(reset.setDate(reset.getDate() + i)),
+				day = {};			
+
+			day["day"] = days[date.getDay()];
+			day["date"] = date.getDate();
+			if (today.getDate() === date.getDate()) day["today"] = true;
+
+			week["dates"].push(day);
+		}
+
+		$('#calendar .month').html(month + ' ' + year);
+
+		var templateScript = $('#templateCalendar').html(),
+			template = Handlebars.compile(templateScript);
+
+		$('#calendar .week').html(template(week));
+
+		// Get pulse data
 		$.get('../data/pulse.json', function(data) {
 			if (data) {
 				var templateScript = $('#templatePulse').html(),
