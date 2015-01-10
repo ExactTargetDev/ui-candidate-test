1c353dd9e6f25c8ec72413cda6c61fc97dc70ee2
neillobo <neillobo@outlook.com>
2015-01-10 14:18:01 -0800

    findMinValue function and test

--- jsanddom.js
+++ jsanddom.js
@@ -11,7 +11,9 @@
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+
+         if (values.length === 0) return null;
+         return Math.min.apply(null, values)
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -13,10 +13,27 @@ test( "Example Test", 2, function() {
 Please create your tests below...
 ********************************/
 module( "Reverse String Unit Test" );
-test( "Reverse String", 2, function() {
+test( "Reverse String", 3, function() {
     // Verify the method exists
     equal( typeof reverseString, 'function', 'Must contain a reverse function' );
 
-    // Make sure the result from the divide function is valid
+    // Make sure the result from the reverseString function is valid
+    ok( reverseString("") === "", 'Expected "" as the result, the result was: ' + reverseString("") );
+
     ok( reverseString("hello world") === "dlrow olleh", 'Expected "dlrow olleh" as the result, the result was: ' + reverseString("hello world") );
 });
+
+module( "Minimum Value Unit Test" );
+test( "Minimum Value", 4, function() {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' );
+
+    //empty array should return null
+    ok( findMinValue([]) === null , 'Expected null as the result, the result was: ' + findMinValue([]) );
+
+    // Make sure the result from the minimum function is valid
+    ok( findMinValue([1,2,3]) == 1 , 'Expected 1 as the result, the result was: ' + findMinValue([1, 2, 3]) );
+
+    ok( findMinValue([-1,-2,-3]) == -3 , 'Expected -3 as the result, the result was: ' + findMinValue([-1, -2, -3]) );
+});
+
