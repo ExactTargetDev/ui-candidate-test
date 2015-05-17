a70406b4ed11c3532921109bd6442bf9abd4de08
Dominic Reitman <dominicreitman@gmail.com>
2015-05-17 19:46:02 -0400

    Added reverseString function and test

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,18 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+
+test("reverseString Test", 5, function() {
+    equal(typeof reverseString, 'function', 'Verify the reverseString function is present');
+
+    ok(reverseString('Nick'), 'kciN', 'Expect a valid result');
+
+    // Even number of characters
+    equal(reverseString('Nick'), 'kciN', 'Check even # Chars');
+
+    // Odd number of characters
+    equal(reverseString('Reitman'), 'namtieR', 'Check odd # Chars');
+
+    // Two Words
+    equal(reverseString('Nick Reitman'), 'namtieR kciN', 'Check with two words');
+});
