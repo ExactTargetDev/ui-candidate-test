d131644cc5ed70f50bf6872832b69e66ad3ed607
Christopher Reynolds <ChristopherRobinReynolds@gmail.com>
2015-05-26 17:07:17 -0400

    pushOntoArray now modified existing array with new data

--- jsanddom.js
+++ jsanddom.js
@@ -61,7 +61,14 @@
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+        //Test data used:
+        //var array = [1, 2, 3, 4, 5];
+        //var toPush = [7, 8, 9];
+
+        console.log("Array: "+array); //Array before
+        array.push(toPush); //Modify by pushing to the original array
+        console.log("Updated Array: "+array); //Log new values for the original array
+
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
