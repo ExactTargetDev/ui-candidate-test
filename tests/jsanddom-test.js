a33a1f7ced0296fe50b8699d7b442b17d8c702b7
daria <pirojenka1@gmail.com>
2015-05-31 21:48:48 +0300

    reversString functionality was added.

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -9,6 +9,10 @@ test( "Example Test", 2, function() {
     // Make sure the result from the divide function is valid
     ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
 });
-/********************************
-Please create your tests below...
-********************************/
+test( "Reverse String Test", 'kcor srebircsbuS', function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+    // Make sure the result from the reverseString function is valid
+    ok( reverseString('Subscribers rock'), 'kcor srebircsbuS', 'Expected "kcor srebircsbuS" as the result, the result was: ' + reverseString('Subscribers rock') );
+});
