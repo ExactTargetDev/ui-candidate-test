fc55e4f8f475ebba3372fb678576262d49d52ca8
gamulya <amulyadamoder@gmail.com>
2015-07-12 01:57:44 -0700

    function to push either a simple value or an array of values onto a specified array

--- jsanddom.js
+++ jsanddom.js
@@ -78,7 +78,14 @@
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
          // FILL THIS IN
-     }
+         if (typeof toPush === 'object') {
+             array.push.apply(array, toPush);
+         } else {
+             array.push(toPush);
+         }
+
+         return array;
+      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
