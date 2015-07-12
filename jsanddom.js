44dfe8a14be48db689a7c21d48f04eba4d560bfe
gamulya <amulyadamoder@gmail.com>
2015-07-12 01:46:33 -0700

    Finding Minimum Value

--- jsanddom.js
+++ jsanddom.js
@@ -13,7 +13,9 @@
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
          // FILL THIS IN
-     }
+         return Math.min.apply(null, values);
+      }
+
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
