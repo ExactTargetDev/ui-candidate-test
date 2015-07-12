478ca50a62c40130262ad98399997e3e534be8d0
gamulya <amulyadamoder@gmail.com>
2015-07-12 01:59:38 -0700

    Function for split string using comma

--- jsanddom.js
+++ jsanddom.js
@@ -90,7 +90,13 @@
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
          // FILL THIS IN
-     }
+		 
+  if (sourceStr == '') {
+    return [];
+  } else {
+    return sourceStr.split (',');
+  }
+}
 
      // Write a function that will take any number of arguments and return their sum
      function sum() {
