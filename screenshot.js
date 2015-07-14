dde14c1aaa5ade37dbb44a1fa659feb599c1c0db
Joshua Sampia <JSampia@corner-enviro.com>
2015-07-14 18:22:18 -0400

    Pressing the help button loads a dialog with the help JSON data. It also builds clickable links.

--- screenshot.js
+++ screenshot.js
@@ -16,4 +16,28 @@ $(function(){
 
 		dialog.html(text).dialog('open');
 	});
+
+	$('a#helpLink').click(function(e){
+		e.preventDefault();
+
+		dialog.html('');
+
+		//Get the JSON data for help.
+		$.get('./data/help.json',function(data){
+			//Build links from the data and attach them to the dialog.
+			var length = data.help.length;
+			var i, url, title, link;
+
+			for(i=0;i<length;i++){
+				url = data.help[i].URL;
+				title = data.help[i].title;
+
+				link = $('<a>').attr('href',url).text(title);
+
+				dialog.append(link).append('<br><br>');
+			}
+
+			dialog.dialog('option',{title: 'HELP'}).dialog('open');
+		},'json');
+	});
 });
