f5167a7423856ce6dd573f6a074da99d48abba87
Joshua Sampia <JSampia@corner-enviro.com>
2015-07-14 17:47:03 -0400

    Added on click functionality to sidebar divs.

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,19 @@
 $(function(){
-	
+	var dialog = $('.dialog');
+
+	dialog.dialog({
+		autoOpen: false,
+		modal: true,
+		title: "Information",
+		width: 300,
+		height: 500
+	});
+
+	$('a.sidebar-nav-link').click(function(e){
+		e.preventDefault();
+
+		var text = $(this).text();
+
+		dialog.html(text).dialog('open').dialog('moveToTop');
+	});
 });
