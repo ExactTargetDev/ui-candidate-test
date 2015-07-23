fc837b86de9e5a2011673d7e2d411b4c0eaff4e9
liza1234 <lizazinto@yahoo.com>
2015-07-23 18:36:31 +0530

    task completed

--- ui-core-utilities.js
+++ ui-core-utilities.js
@@ -35,4 +35,25 @@ var unitTestJson ={
      i : '',
      j : [1, 1, 2, 3, 5, 7],
      k : ' '
-}
+};
+
+(function() {
+  // Union of Chrome, Firefox, IE, Opera, and Safari console methods
+  var methods = ["assert", "assert", "cd", "clear", "count", "countReset",
+    "debug", "dir", "dirxml", "dirxml", "dirxml", "error", "error", "exception",
+    "group", "group", "groupCollapsed", "groupCollapsed", "groupEnd", "info",
+    "info", "log", "log", "markTimeline", "profile", "profileEnd", "profileEnd",
+    "select", "table", "table", "time", "time", "timeEnd", "timeEnd", "timeEnd",
+    "timeEnd", "timeEnd", "timeStamp", "timeline", "timelineEnd", "trace",
+    "trace", "trace", "trace", "trace", "warn"];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+  var method;
+  var noop = function() {};
+  while (length--) {
+    method = methods[length];
+    // define undefined methods as noops to prevent errors
+    if (!console[method])
+      console[method] = noop;
+  }
+})();
