5eb4ad4bf4b07269095257541c817c6f0bab1c6b
rangachanakya <rangachanakyagade@gmail.com>
2015-07-27 13:53:43 -0500

    Update jsanddom-test.js

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,13 +1,87 @@
 /********************************
 Unit Test Example
 ********************************/
-module( "Example Unit Test" );
-test( "Example Test", 2, function() {
-    // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+module( "Example Unit Test" );   module( "Example Unit Test" );
+ test( "Example Test", 2, function() {   test( "Example Test", 2, function() {
+     // Verify the method exists       // Verify the method exists
+     equal( typeof divide, 'function', 'Must contain a divide function' );        equal( typeof divide, 'function', 'Must contain a divide function' ); 
+    
+     // Make sure the result from the divide function is valid       // Make sure the result from the divide function is valid
+    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );  + // >>>>> this example was wrong.  I changed below - "divide(4,2), 2,"  to "divide(4,2) == 2," 
+    ok( divide( 4, 2 ) == 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+ });
+// fix IE8 console problem  ///////
+if(!window.console) {console = {log: function() {}} };////////////////////////////////////
 
-    // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+test('reverse string', 2, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' ); 
+ 
+ ok( reverseString('abcd') == 'dcba', 'Expected dcba as the result, the result was '+reverseString('abcd'));
+});
+           
+         
+test('find min value', 2, function() {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' ); 
+ 
+ ok( findMinValue([40,28,9,320,46]) == 9, 'Expected 9 as the result, the result was '+findMinValue([40,28,9,320,46]) );
+});
+
+test('return distinct values', 2, function() {
+    // Verify the method exists
+    equal( typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function' ); 
+    
+    ok( JSON.stringify(findDistinctValues(['a',3,1,1,'a','bad',34,1,54,28,'bad',54])) == JSON.stringify([1,28,3,34,54,'a','bad']), 
+    'Expected [1,28,3,34,54,a,bad] as the result, the result was '+findMinValue(['a',3,1,1,'a','bad',34,1,54,28,'bad',54]) );
+});
+ 
+
+test('print divisible by 3 and 5', 2, function() {
+    // Verify the method exists
+    equal( typeof doFizzBuzz, 'function', 'Must contain a doFizzBuzz function' ); 
+    
+    var f = doFizzBuzz();
+    ok( f[2] == 'Fizz' && f[4] == 'Buzz' && f[14] == 'FizzBuzz' && f[7] == 8 && f.length == 100, 
+    'Expected true as the result, the result was false');
+});
+
+
+test('remove fruits', 3, function() {
+    // Verify the method exists
+    equal( typeof removeFruits, 'function', 'Must contain a removeFruits function' ); 
+    
+    var f = removeFruits(['apple', 'orange', 'grapes', 'melon'],['grapes','lemon', 'orange']);
+    ok(f.length == 2 && $.inArray('apple',f) >= 0 && $.inArray('melon',f) >= 0,
+       'Expected true as the result, the result was false');
+    
+    ok(removeFruits('apple', ['apple']) == false,
+      'Expected  false, the result was not false');
+});
+
+test('push onto array', 3, function() {
+    // Verify the method exists
+    equal( typeof pushOntoArray, 'function', 'Must contain a pushOntoArray function' ); 
+    
+    var array = ['apple', 'orange', 'grapes'];
+    pushOntoArray(array, ['fig','melon'])
+    ok(JSON.stringify(array) == JSON.stringify(['apple', 'orange', 'grapes','fig','melon']),
+       'Expected true as the result, the result was false');
+   
+    pushOntoArray(array, 'cranberry');
+    ok(JSON.stringify(array) == JSON.stringify(['apple', 'orange', 'grapes','fig','melon','cranberry']),
+      'Expected  true, the result was false');
+});
+test('split String using comma', 3, function() {
+    // Verify the method exists
+    equal( typeof splitListStrUsingComma, 'function', 'Must contain a splitListStrUsingComma function' ); 
+    
+    var SourceStr = 'Ranga,chanakya';
+    var resultStr = splitListStrUsingComma(sourceStr);
+	ok( resultStr[0] =='Ranga'&& resultStr[1]=='Chanakya', 
+    'Expected true as the result, the result was false');
+   
+
 });
 /********************************
 Please create your tests below...
