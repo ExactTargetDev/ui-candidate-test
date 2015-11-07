cad9e053faa0319f260206ba34ec0aff4ec3e18f
danodea <danodea@gmail.com>
2015-11-07 13:45:10 -0500

    Create pushOntoArray

--- jsanddom.js
+++ jsanddom.js
@@ -69,7 +69,12 @@
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+        //Duck-typing to check if toPush is an array
+        if (toPush.length) {
+            Array.prototype.push.apply(array, toPush);
+        } else {
+            array.push(toPush);
+        }
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
