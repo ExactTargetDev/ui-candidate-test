bca1d66558d589c3b20fb43dce1b5cba3c29c6d5
danodea <danodea@gmail.com>
2015-11-09 08:43:51 -0500

    Create help modal
    In Chrome, it only works if you start a local HTTP server due to security measures

--- screenshot.js
+++ screenshot.js
@@ -3,4 +3,17 @@ $(function() {
 		$('.panel').removeClass('medium');
 		$(this).addClass('medium');
 	});
+
+	$('#help').click(getHelp);
+
+	function getHelp() {
+		$.getJSON('data/help.json', function(data) {
+			var $modalList = $('<ul>');
+			data.help.forEach(function(element) {
+				$modalList.append('<li><a href="' + element.URL + '">' + element.title + '</a></li>');
+			});
+
+			$modalList.dialog({modal: true, title: 'HELP!', position: {my: 'right top', at: 'bottom', of: $('#help')}});
+		});
+	}
 });
