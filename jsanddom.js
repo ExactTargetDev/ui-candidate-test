04461a2e2236b5423645f1fa45376ec2fa5a4e77
marcieknuteson <marcieroberts22@gmail.com>
2015-11-24 17:43:32 -0500

    Push Onto Array
    created function Push Onto Array

--- jsanddom.js
+++ jsanddom.js
@@ -64,12 +64,13 @@
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+         array.push(toPush);
+         return array;
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
-         // FILL THIS IN
+         
      }
 
      // Write a function that will take any number of arguments and return their sum
