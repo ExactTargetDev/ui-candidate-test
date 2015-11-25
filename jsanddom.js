608850cc07d8f630caec5a073266b2ae992e7b5b
marcieknuteson <marcieroberts22@gmail.com>
2015-11-24 19:49:44 -0500

    jQuery selectors
    5 different jQuery selections for the sample anchor

--- jsanddom.js
+++ jsanddom.js
@@ -169,6 +169,11 @@
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
+     $(".link");
+     $("#fizz");  
+     $(".buzz.link"); 
+     $("a");
+     $(".link a");
 
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
