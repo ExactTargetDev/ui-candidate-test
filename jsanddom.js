35f8b19e9f8e5632dffd25164b4cb29096a2bc1d
Hirschy, Kreigh <Kreigh.Hirschy@inin.com>
2015-12-07 23:33:17 -0500

    Test App

--- jsanddom.js
+++ jsanddom.js
@@ -1,22 +1,27 @@
      // Example unit test function
      function divide( a, b ) {
         // To see the test pass, uncomment the following line
-        //return a / b;
+        return a / b;
      }
 
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+         return str.split('').reverse().join('');
      }
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+         return Math.min.apply(null, values);
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
          // FILL THIS IN
+          var filteredArray = values.filter(function(elem, pos){
+               values.indexOf(elem) == pos;
+          });
+
+          return filteredArray;
      }
 
      // Write a function that logs the numbers from 1 to 100 to the console.
@@ -25,6 +30,24 @@
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
          // FILL THIS IN
+          for(var i = 0; i <= 100; i++){
+               if ((i % 3 === 0) && (i % 5 === 0)) {
+                    console.log('FizzBuzz');
+                    return 'FizzBuzz';
+               }
+               else if (i % 3 === 0) {
+                    console.log('Fizz');
+                    return 'Fizz';
+               }
+               else if (i % 5 === 0) {
+                    console.log('Buzz');
+                    return 'Buzz';
+               }
+               else {
+                    console.log(i);
+                    return i;
+               }
+          }
      }
 
      // You have a master array of strings, where each element is a fruit name.
@@ -33,6 +56,12 @@
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
      function removeFruits(fruits, fruitsToRemove) {
          // FILL THIS IN
+          for(var i = 0; i <= fruits.length; i++){
+               if(fruits[i] == fruitsToRemove){
+                    fruits.splice(fruitsToRemove, fruitsToRemove.length);
+               }
+          }
+          return fruits;
      }
 
      // Write a function to push either a simple value or an array of values onto a specified array.
@@ -41,24 +70,51 @@
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
          // FILL THIS IN
+          if(toPush.length > 1){
+               for(var i=0; i <= toPush.length; i++){
+                    array.push(toPush[i]);
+               }
+          } else{
+               array.push(toPush);
+          }
+
+          return array;
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
          // FILL THIS IN
+          if(sourceStr){
+               return sourceStr.split('').join(',');
+          } else{
+               return [];
+          }
      }
 
      // Write a function that will take any number of arguments and return their sum
      function sum() {
          // FILL THIS IN
+          var sum = 0;
+          for(var i = 0; i < arguments.length; i++){
+               sum += arguments[i];
+          }
+          return sum;
      }
 
      // Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
          // FILL THIS IN
+          if (!sourceStr.replace(/\s/g, '').length) {
+               return true;
+          }
      }
 
      // write an example of a javascript closure
+     function makeColor(color){
+          return function(){
+               document.body.style.color = color;
+          }
+     }
 
      // define a json object that represents a collection of people.
      // each person should have the following properties
@@ -68,6 +124,7 @@
      // - state
      // - zip
      // - a collection of phone numbers (home, work, mobile)
+     var collection = [{firstName: 'Kreigh', lastName: 'Hirschy', city: 'Indianapolis', state: 'Indiana', zip: 46563, phoneNumbers: [{home: '574-952-3414'}, {work: '317-957-1782'}, {mobile: '574-952-3414'}]},{firstName: 'Timmy', lastName: 'Tommy', city: 'Indianapolis', state: 'Indiana', zip: 46563, phoneNumbers: [{home: '317-222-2222'}, {work: '317-222-2222'}, {mobile: '317-222-2222'}]}];
 
 
      // Create a javascript object (DataTable) with the following:
@@ -85,14 +142,59 @@
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
+     var options = [
+          {val: 'kreigh', name: 'Kreigh'},
+          {val: 'ryan', name: 'Ryan'},
+          {val: 'larry', name: 'Larry'}
+     ];
+
+     var sel = $('<select id="select">').appendTo('#div1');
+     var btn = $('<button>Submit</button>').appendTo('#div1');
+
+     $(options).each(function(){
+          sel.append($('<option>').attr('value', this.val).text(this.name));
+     });
+
+     $(btn).click(function(){
+          var value = $('#select').val();
+          var name = $('#select option:selected').text();
+          console.log('name: ' + name + ' val: ' + value);
+     });
+
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
 
+
+     //1. $('#fizz a')
+     //2. $('#foo a')
+     //3. $('body a')
+     //4. $('#div1').next().find('a')
+     //5. $('#baz').prev().find('a')
+
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
+     var array = ['apple', 'berry', 'grape', 'orange', 'kiwi'];
+
+     for(var i =0; i < array.length; i++){
+          $('#list1').append('<li>'+array[i]+'</li>');
+     }
 
      // Use javascript to add a list of checkboxes and 2 links
      // to the div with an id of "foobar"
      // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
      // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
+     var container = $('#foobar');
+
+     container.append('<input type="checkbox" /><br />');
+     container.append('<input type="checkbox" /><br />');
+     container.append('<a href="#">Check all</a><br />');
+     container.append('<a href="#">Uncheck all</a>');
+
+     $('#foobar a:first-of-type').click(function(){
+          $("input[type='checkbox']").prop('checked', true);
+     });
+
+     $('#foobar a:last-of-type').click(function(){
+          $("input[type='checkbox']").prop('checked', false);
+     });
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,80 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+test('Reverse Test', 2, function(){
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function');
+
+    ok(reverseString('hello?'), '?olleh', 'Expected ?olleh as the result, the result was: ' + reverseString('hello?'));
+});
+
+test('Get minimum values', 2, function(){
+    equal(typeof findMinValue, 'function', 'Must contain a findMinValue function');
+
+    var array = [1, 2, 3, 4, 6, 7];
+
+    ok(findMinValue(array), 1, 'Expected 1 as the result, the result was: ' + findMinValue(array));
+});
+
+test('Get distinct values', 2, function(){
+    equal(typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function');
+
+    var dupArray = ['test', 'test1', 'test2', 'test'];
+
+    var filter = ['test', 'test1', 'test2'];
+    ok(findDistinctValues(dupArray), filter, 'Expected "test", "test1", "test2", the result was ' + findDistinctValues(dupArray));
+});
+
+test('Do FizzBuzz', 4, function(){
+    equal(typeof doFizzBuzz, 'function', 'Must contain a FizzBuzz Function');
+
+    ok(doFizzBuzz(), 'Fizz', 'Expected Fizz, the result was ' + doFizzBuzz());
+
+    ok(doFizzBuzz(), 'Buzz', 'Expected Buzz, the result was ' + doFizzBuzz());
+
+    ok(doFizzBuzz(), 'FizzBuzz', 'Expected FizzBuzz the result was ' + doFizzBuzz());
+});
+
+test('Remove Fruits', 2, function(){
+    equal(typeof removeFruits, 'function', 'Must contain a removeFruits function');
+
+    var fruits = ['pinapple', 'apple', 'grapes', 'kiwi', 'lemon', 'orange'];
+    var fruitsToRemove = ['pinappe', 'kiwi', 'lemon'];
+
+    var expect = ['apple', 'grapes', 'orange'];
+    ok(removeFruits(fruits, fruitsToRemove), expect, 'Expected ' + expect + ', the result was ' + removeFruits(fruits, fruitsToRemove));
+});
+
+test('Push Onto Array', 2, function(){
+    equal(typeof pushOntoArray, 'function', 'Must contain a pushOntoArray function');
+
+    var array = [0, 1, 2];
+    var expect1 = [0, 1, 2, 4];
+
+    ok(pushOntoArray(array, 4), expect1, 'Expected ' + expect1 + ', the result was ' + pushOntoArray(array, 4));
+});
+
+test('Split List String Using Comma', 3, function(){
+   equal(typeof splitListStrUsingComma, 'function', 'Must contain a splitListStrUsingComma function');
+
+    ok(splitListStrUsingComma('hello'), 'h,e,l,l,o', 'Expected h,e,l,l,o , the result was ' + splitListStrUsingComma('hello'));
+
+    ok(splitListStrUsingComma(''), '[]', 'Expected [] , the result was ' + splitListStrUsingComma(''));
+});
+
+test('Should add up the sum', 2, function(){
+    equal(typeof sum, 'function', 'Must contain a sum function');
+
+    ok(sum(1, 4, 6, 10), 21, 'Expected 21, the result was ' + sum(1, 4, 6, 10));
+});
+
+test('Is only whitespace', 2, function(){
+    equal(typeof isOnlyWhitespace, 'function', 'Must contain a isOnlyWhitespace function');
+
+    ok(isOnlyWhitespace('   '), true, 'Expected true, the result was ' + isOnlyWhitespace(''));
+});
+
+test('It should make green', 2, function(){
+    equal(typeof makeColor, 'function', 'Must contain a makeColor function');
+
+    ok(makeColor('green'), true, 'Expected true, the result was ' + makeColor('green'));
+});
