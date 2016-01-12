accb52bb2e73644651af09b64013fd62bf59c2e7
Saurav Bhalotia <bhalotia.saurav@gmail.com>
2016-01-12 11:37:03 -0500

    Winding-Up with major requiremnts on ScreenShot replicate

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 $.getJSON("data/help.json", function(json) {
-    console.log(json);
+    $.each(json, function(idx, helpArray) {
+        var i, key, _key, _value, link,
+            ul = $('<ul>');
+
+        for (i = 0; i < helpArray.length; i++) {
+            link = $('<li>').append($('<a>', { className: 'url-link', href: helpArray[i].URL, 'target': '_blank'} )
+                            .append('<span className = "title">'+helpArray[i].title+'<span/>'));
+            ul.append(link);
+        }
+        $('.modal .modal-body').append(ul);
+    });
 });
 
-$("aside ul>li").on("click", 'a', function(event){
+$("aside ul>li").on("click", 'a', function(event) {
     event.preventDefault();
     var $this = $(this).parent();
     $("aside ul>li").removeClass("active");
     $this.addClass("active");
-    console.log('this - ' + $this);
 });
 
-$('#helpJSON').on('click', 'a', function(event){
-    // $('.helpWindow').removeClass('x-hidden');
-    console.log('event');
+$("div.content > ul").on("click", "li", function(event){
+    var $this = $(this);
+    $this.find('displayOnExpand').removeClass('x-hidden');
+    $this.addClass("expand").siblings().removeClass("expand");
 });
