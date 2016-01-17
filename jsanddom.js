9e6cfedf0dd9228d375ccea78fc205e0a24fbe7f
Matt Zelenak <matt.e.zelenak@gmail.com>
2016-01-17 11:48:34 -0500

    first pass of pushOntoArray
    no unit test written for this logic yet

--- jsanddom.js
+++ jsanddom.js
@@ -66,7 +66,12 @@ function removeFruits(fruits, fruitsToRemove) {
 // If toPush is a simple value, it should be pushed onto array as an element.
 // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
 function pushOntoArray(array, toPush) {
-   // FILL THIS IN
+  if(Object.prototype.toString.call(toPush) === "[object Array]"){
+    array.push.apply(array, toPush);
+  } else {
+    array.push(toPush);
+  }
+  return array;
 }
 
 // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
