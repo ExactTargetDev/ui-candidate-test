ad4a8bb6c08e73f82bbf0fa4e30bf5d6347a924e
Matt Zelenak <matt.e.zelenak@gmail.com>
2016-01-17 14:05:01 -0500

    cleaning up some test logic, findDistinctValues unit test pass
    I had some funky logic copied from the test example that was marring the actual result of each unit test

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,14 +1,14 @@
 /********************************
 Unit Test Example
 ********************************/
-module( "Example Unit Test" );
-test( "Example Test", 2, function() {
-    // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+// module( "Example Unit Test" );
+// test( "Example Test", 2, function() {
+//     // Verify the method exists
+//     equal( typeof divide, 'function', 'Must contain a divide function' ); 
 
-    // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
-});
+//     // Make sure the result from the divide function is valid
+//     ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+// });
 
 /********************************
 Please create your tests below...
@@ -17,11 +17,17 @@ Please create your tests below...
 module( "reverseString Unit Test" );
 test( "reverseString Test", function() {
     equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
-    ok( reverseString("Subscribers rock"), "kcor srebircsbuS", 'Expected "kcor srebircsbuS" as the result, the result was: ' + reverseString("Subscribers rock") );
+    ok( reverseString("Subscribers rock"), 'Expected kcor srebircsbuS as the result, the result was: ' + reverseString("Subscribers rock") );
 });
 
 module( "findMinValue Unit Test" );
 test( "findMinValue Test", function() {
     equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' );
-    ok( findMinValue([1, 10, 100]), 1, 'Expected 1 as the result, the result was: ' + findMinValue([1, 10, 100]) );
+    ok( findMinValue([1, 10, 100]), 'Expected 1 as the result, the result was: ' + findMinValue([1, 10, 100]) );
+});
+
+module( "findDistinctValues Unit Test" );
+test( "findDistinctValues Test", function() {
+    equal( typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function' );
+    ok( findDistinctValues([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]), 'Expected 3, 5, 7, 20, 0.18, 1, -1.1, 12, Infinity as the result, the result was: ' + findDistinctValues([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]) );
 });
