cc1c2814de3e1ea60b357f52b6e5a67fc93f9244
Matt Zelenak <matt.e.zelenak@gmail.com>
2016-01-17 11:41:45 -0500

    first pass of fruits to remove func
    no unit test created for this logic yet

--- jsanddom.js
+++ jsanddom.js
@@ -51,7 +51,14 @@ function doFizzBuzz() {
 // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
 // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
 function removeFruits(fruits, fruitsToRemove) {
-   // FILL THIS IN
+  for(var a = 0; a < fruits.length; a++){
+    for(var b = 0; b < fruitsToRemove.length; b++){
+      if(fruits[a] === fruitsToRemove[b]){
+        fruits.splice(a, 1);
+      }
+    }
+  }
+  return fruits;
 }
 
 // Write a function to push either a simple value or an array of values onto a specified array.
