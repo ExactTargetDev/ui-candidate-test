81454a4666c24bf87142ec4809cd5098d98a646b
Matt Zelenak <matt.e.zelenak@gmail.com>
2016-01-18 13:33:52 -0500

    final touches, testing
    had some final styles to apply, some icons to add, had to fiddle with the js a bit. I did some testing in latest versions of Firefox and Chrome, and fixed a bug I saw in the js for Firefox. Getting ready to set up bootcamp on this machine so I can test the code in IE.

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -1,10 +1,52 @@
 $(document).ready(function(){
+	
+	// make sure only one pulse bubble is in medium view at any given time
 	$("li.bubble").click(function(){
-		console.log("bubble clicked");
-		console.log(this);
-		$(".large").hide();
-		$(".small").show();
-		$(this).find(".large").show();
-		$(this).find(".small").hide();
+		$(".large").hide(50);
+		$(".small").show(50);
+		$(this).find(".large").show(50);
+		$(this).find(".small").hide(50);
+	});
+
+	// json pulled from data/help.json
+	var json = {
+  "help": 
+  	[
+	    {
+	      "title": "What is JSON?",
+	      "URL": "http://en.wikipedia.org/wiki/JSON"
+	    },
+	    {
+	      "title": "What is ExactTarget?",
+	      "URL": "http://en.wikipedia.org/wiki/ExactTarget"
+	    },
+	    {
+	      "title": "More info about ExactTarget REST APIs",
+	      "URL": "http://code.exacttarget.com/question/rest-api-authentication"
+	    }
+	  ]
+	};
+
+	// logic for showing the help overlay modal
+	$("#help-button").click(function(){
+		$("#help-modal").show(10, function(){
+			for(var i = 0; i < json.help.length; i++){
+				console.log(i);
+				var link = document.createElement("a");
+				link.href = json.help[i].URL;
+				link.innerHTML = json.help[i].title;
+				link.className = "jsonLink";
+				console.log(link);
+				$("#modal-bubble").append(link);
+			}
+		});
+	});
+
+	// exit sequence for help modal
+	$("#modal-exit").click(function(){
+		$("#help-modal").hide(10, function(){
+			$(this).find('a.jsonLink').remove();
+		});
 	});
 });
+
