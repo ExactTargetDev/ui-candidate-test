ca5040cf2c15a832a0c8ff94dd7586caf90774e6
Matt Zelenak <matt.e.zelenak@gmail.com>
2016-01-18 10:18:13 -0500

    click states for pulse bubbles correctly showing / hiding only one at a time

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+	$("li.bubble").click(function(){
+		console.log("bubble clicked");
+		console.log(this);
+		$(".large").hide();
+		$(".small").show();
+		$(this).find(".large").show();
+		$(this).find(".small").hide();
+	});
+});
