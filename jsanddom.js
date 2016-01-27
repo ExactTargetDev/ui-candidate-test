e652ee93edad87a6c4dbddf71061994a3a6d8383
Jeremy Wagner <jeremywagner15@gmail.com>
2016-01-27 15:23:38 -0500

    Adds findMinValue function and passing 'findMinValue Unit Test'

--- jsanddom.js
+++ jsanddom.js
@@ -11,7 +11,7 @@
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+         return Math.min.apply(Math, values);
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -21,3 +21,17 @@ test( "reverseString Test", 2, function() {
 	// Make sure the result from the reverseString function is valid
 	equal( reverseString("Subscribers rock"), "kcor srebircsbuS", "Expected kcor srebircsbuS as the result, the result was: " + reverseString("Subscribers rock"));
 });
+
+/********************************
+Test findMinValue
+********************************/
+module( "findMinValue Unit Test" );
+test( "findMinValue Test", 2, function() {
+	// Verify the method exists
+	equal( typeof findMinValue, "function", "Must contain a findMinValue function" );
+
+	var testData = [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12];
+
+	// Make sure the result from the findMinValue function is valid
+	equal( findMinValue(testData), -1.1, "Expected -1.1 as the result, the result was: " + findMinValue(testData));
+});
