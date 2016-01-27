524eca5e2a3a1ed47d9cb48d77ebdab942f74811
Jeremy Wagner <jeremywagner15@gmail.com>
2016-01-27 18:37:34 -0500

    adds createFiveItems function

--- jsanddom.js
+++ jsanddom.js
@@ -218,8 +218,17 @@
     $(".bar a");
     $(".buzz a");
     $("a");
+
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
+     function createFiveItems(){
+        var ul = document.getElementById("list1");
+        for(var i = 0; i < 5; i++){
+            var li = document.createElement("li");
+            li.innerText = i;
+            ul.appendChild(li);
+        }
+     }
 
      // Use javascript to add a list of checkboxes and 2 links
      // to the div with an id of "foobar"
