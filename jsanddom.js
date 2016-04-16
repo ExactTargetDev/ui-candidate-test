aaf6bf5a47ccf671bacabbaf23e664e3c359749c
Lars <larsbell@gmail.com>
2016-04-16 06:04:41 -0800

    Adds standard tests to min value and reverse string updates misvalue to math.

--- jsanddom.js
+++ jsanddom.js
@@ -11,9 +11,7 @@
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
-//        return math.min(values);
-       return values.sort()[0];
+       return Math.min.apply(null, values);
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -14,21 +14,25 @@ Please create your tests below...
 ********************************/
 
 module ( "reverseString" );
-test( "Reverse String unit test", 4, function () {
+test( "Reverse String unit test", 5, function () {
   equal ( typeof reverseString, 'function', "Must contain a reverse string function" );
   equal ( reverseString( "1234"), "4321", "Expected 1234 to return 4321");
   equal ( reverseString( "straw" ), "warts", "Expected straw to return warts" );
   notEqual ( reverseString( "" ), "something else", "Expected the empty string to NOT return the string something else");
+  equal ( reverseString ( "Subscribers rock"), "kcor srebircsbuS", "Expected Subscribers rock to return kcor srebircsbuS" );
 });
 
 module ( "findMinValue");
-test( "Find numeric minimum values", 6, function (){
+test( "Find numeric minimum values", 7, function (){
   equal ( findMinValue([1,2]), 1, "Expected the min value of 1,2 to be 1");
   equal ( findMinValue([1,-2]), -2, "Expected the min value of 1,-2 to be -2");
   equal ( findMinValue([5,5]), 5, "Expected the min value of 5,5 to be 5");
   equal ( findMinValue([0,5]), 0, "Expected the min value of 0,5 to be 0");
   equal ( findMinValue([3.14,8776464]), 3.14, "Expected the min value of 3.14,8776464 to be 3.14");
-  equal ( findMinValue([2,5,7]), 2, "Expected the min value of 2,5,7 to be 2");
+  equal ( findMinValue([2,5,7,11]), 2, "Expected the min value of 2,5,7,11 to be 2");
+  equal ( findMinValue([3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12]), -1.1, "Expected the min vlue of [3, 5, 7, 20, .18, 01, -1.1, 12, Infinity, Infinity, 0.18, -1.1, 12] to be 01.1");
 });
+
+
 
 
