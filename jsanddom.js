ebb651114902f4651a7f1ea2e4a749c7d4a89ea5
Lars <larsbell@gmail.com>
2016-04-16 06:55:52 -0800

    adds distinct value function.  Adds lodash library. reformats to make spacing consistent

--- jsanddom.js
+++ jsanddom.js
@@ -16,7 +16,7 @@
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
-         // FILL THIS IN
+         return _.uniq(values);
      }
 
      // Write a function that logs the numbers from 1 to 100 to the console.
