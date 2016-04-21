24410546e536c7a9ebe1d55ef3a6b46c65fa3bb0
Sam Sneddon <ssneddon@Cottonmouth-2.local>
2016-04-21 09:23:12 -0400

    Started first by adding a new bootstrap page to work on my dashboard portion of the test. Then I went about adding relative paths for all my libs I've been working with and will be working with. I added a carousel plugin for the Pulse window. I'm not crazy about adding extra plugins if I can help it, but time crunch. Plus, the Slick carousel is pretty functional on mutliple browsers

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+			$('.theSlider').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 300,
+		  slidesToShow: 5,
+		  centerMode: true,
+		  variableWidth: true
+		});
+})
