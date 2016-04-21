4427ea68bd24b9a2b596863f91fbc6dd947d6a13
Sam Sneddon <ssneddon@Cottonmouth-2.local>
2016-04-21 14:43:05 -0400

    added the click functionality to the interactive marketing hub links. worked a little more styling intot the dashboard page. now for parsing some json

--- js/main.js
+++ js/main.js
@@ -16,5 +16,24 @@ $(document).ready(function(){
 			$(".sliderContent").not($(this)).animate({"width": '200px', "height":"100px", "margin-top":"4rem"});
 	});
 
+	$(".marketHubText a").click(function() {
+			$(this).css({"background-color":"#fcaa40","color":"white", "font-size":"1.1rem"});
+			$(".marketHubText a").not($(this)).removeAttr("style");
+	});
+
+	$("#helpData").click(function(){
+		$.getJSON( "data/help.json", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( $(".modal-body") );
+		});
+		$('#myModal').modal();
+	});
 
 });
