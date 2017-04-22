0384ea97b67e9df0552ea0dc7533d6d459206f6a
Demetra Koras <demms@me.com>
2017-04-22 16:59:41 -0400

    Adds FizzBuzz function and tests

--- jsanddom.js
+++ jsanddom.js
@@ -18,7 +18,7 @@
      function findDistinctValues(values) {
         //  return [...new Set(values)]
         arr = []
-        values.forEach( function(value){
+        values.forEach(function(value) {
           if (!arr.includes(value)) {
             arr.push(value)
           }
@@ -31,7 +31,23 @@
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
-         // FILL THIS IN
+       arr = []
+       for (var i = 1; i <= 100; i++) {
+         if (i % 15 == 0) {
+           arr.push("FizzBuzz")
+           console.log("FizzBuzz")
+         }else if (i % 3 === 0) {
+           arr.push("Fizz")
+           console.log("Fizz")
+         }else if (i % 5 === 0) {
+           arr.push("Buzz")
+           console.log("Buzz")
+         }else {
+           arr.push(i)
+           console.log(i)
+         }
+       }
+       return arr
      }
 
      // You have a master array of strings, where each element is a fruit name.
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -32,3 +32,17 @@ QUnit.test( "Distinct Values Test", function(assert) {
 
     assert.ok( findDistinctValues(arr), [3, 5, 7, 20, 0.18, 1, -1.1, 12, Infinity], 'Expected [3, 5, 7, 20, 0.18, 1, -1.1, 12, Infinity] as the result, the result was: ' + findDistinctValues(arr) );
 });
+
+QUnit.test( "FizzBuzz Test", function(assert) {
+    assert.equal( typeof doFizzBuzz, 'function', 'Must contain a FizzBuzz function' );
+
+    assert.ok( doFizzBuzz().length, 100, 'Expected 100 as the result, the result was: ' + doFizzBuzz().length );
+
+    assert.ok( doFizzBuzz(2), 'Fizz', 'Expected Fizz as the result, the result was: ' + doFizzBuzz(2) );
+
+    assert.ok( doFizzBuzz(14), 'FizzBuzz', 'Expected FizzBuzz as the result, the result was: ' + doFizzBuzz(14) );
+
+    assert.ok( doFizzBuzz(19), 'Buzz', 'Expected Buzz as the result, the result was: ' + doFizzBuzz(19) );
+
+    assert.ok( doFizzBuzz(25), 26, 'Expected 26 as the result, the result was: ' + doFizzBuzz(25) );
+});
